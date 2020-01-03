f350197d46f3d6a48ca3d0ce143e2cb2767bf69d
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-03 19:40:31 +0200

    Create ExtraTerrestrials.c

---
+++ extra-terristrials/ExtraTerrestrials.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char word[80];
+	fgets(word, 80, stdin);
+	
+	int i = strlen(word);
+  while (i-->0) 
+    printf("%c", word[i]);
+	  
+  return 0;
+}
