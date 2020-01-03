647ebe90521f920f02f23c5abed0f2255e566e08
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-03 22:13:28 +0200

    Create TheSpyLife.c

---
+++ the-spy-life/TheSpyLife.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define MAX 200
+
+int main() {
+  char message[MAX];
+  fgets(message, MAX, stdin);
+  
+  for (int i = strlen(message); i--; ) {
+  	 char c = message[i];
+  	 if (isalpha(c) || isspace(c)) putchar(c);
+  }
+    
+  return 0;
+}
