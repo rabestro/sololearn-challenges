ec3a35bfdf102f8c3e1dcaea4d76ce527804efe5
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-03 20:17:55 +0200

    Update ExtraTerrestrials.c

--- extra-terristrials/ExtraTerrestrials.c
+++ extra-terristrials/ExtraTerrestrials.c
@@ -2,12 +2,11 @@
 #include <string.h>
 
 int main() {
-	char word[80];
-	fgets(word, 80, stdin);
+  char word[80];
+  fgets(word, 80, stdin);
 	
-	int i = strlen(word);
-  while (i-->0) 
-    printf("%c", word[i]);
+  int i = strlen(word);
+  while (i-->0) printf("%c", word[i]);
 	  
   return 0;
 }
