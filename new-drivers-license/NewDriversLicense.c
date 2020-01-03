2e38ebbdc409637f2c8f1cc0ed6ca60da620d0b4
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-03 18:14:01 +0200

    Create NewDriversLicense.c

---
+++ new-drivers-license/NewDriversLicense.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char name[200];
+	int agents;
+	char persons[4][200];
+	scanf("%s", name);
+	scanf("%d", &agents);
+	scanf("%s %s %s %s", persons[0], persons[1], persons[2], persons[3]);
+  
+	int position = 0;
+	for (int i = 4; i--> 0; ) 
+		if (strcmp(name, persons[i]) > 0) 
+			++position;
+      
+	printf("%d", 20 + 20 * (int)(position / agents));
+  return 0;
+}
