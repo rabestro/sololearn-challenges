c5d107696968d9ef542947bd57e2797b9f852c87
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-03 19:04:21 +0200

    Update JungleCamping.c

--- jungle-camping/JungleCamping.c
+++ jungle-camping/JungleCamping.c
@@ -5,13 +5,13 @@ int main() {
   char *animals[] = {"Lion", "Tiger", "Snake", "Bird"};
   char *sounds[] = {"Grr", "Rawr", "Ssss", "Chirp"};
    
-	char noise[500];
+  char noise[500];
   fgets(noise, 500, stdin);
   
   for (char *sound = strtok(noise, " "); sound; sound = strtok(NULL, " ")) 
-   	for (int i = 0; i < 4; ++i)
-   		if (strcmp(sound, sounds[i]) == 0)
-   			printf("%s ", animals[i]);
+    for (int i = 0; i < 4; ++i)
+      if (strcmp(sound, sounds[i]) == 0)
+   	printf("%s ", animals[i]);
    
    return 0;
 }
