75fbfd8e72e0440c56c5c36d6ef845b937cdf1eb
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-04 00:01:08 +0200

    Create YouTubeLinkFinder.c

---
+++ youtube-link-finder/YouTubeLinkFinder.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 200
+
+int main() {
+  char url[MAX];
+  fgets(url, MAX, stdin);
+  
+  char *p = url + strlen(url);
+  while (*p != '=' && *p != '/' && p != url) --p;
+  puts(++p);
+  
+  return 0;
+}
