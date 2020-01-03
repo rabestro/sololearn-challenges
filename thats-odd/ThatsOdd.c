00a7e064beacf2247bf3eaf003adcc94b5d8b93f
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-03 22:39:53 +0200

    Create ThatsOdd.c

---
+++ thats-odd/ThatsOdd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int count;
+  scanf("%d", &count);
+  
+  int summa = 0;
+  while (count--) {
+  	 int number;
+  	 scanf("%d", &number);
+  	 if (number % 2 == 0) summa += number;
+  }
+   
+  printf("%d", summa);
+  return 0;
+}
