c0ea5b88d7ed7640ec5ddbab09cdd7f0b6e6ba87
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-03 21:41:17 +0200

    Create PigLatin.c

---
+++ pig-latin/PigLatin.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 200
+
+int main() {
+  char sentence[MAX];
+  fgets(sentence, MAX, stdin);
+  
+  for (char *word = strtok(sentence, " "); word; word = strtok(NULL, " ")) 
+  	 printf("%s%cay ", word + 1, *word);
+  
+  return 0;
+}
