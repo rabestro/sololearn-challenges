353211b224bbbb672b99e566d1069f92d450c3f9
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-04 15:26:28 +0200

    Create NoNumerals.c

---
+++ no-numerals/NoNumerals.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 200
+
+char *numberToWord(char *word) {
+  char const *numbers[] = {"10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0"};
+  char const *words[] = {"ten", "nine", "eight", "seven", "six", "five", "four", "three", "two", "one", "zero"};
+
+  for (int i = 0; i < 11; ++i)
+    if (strcmp(word, numbers[i]) == 0) 
+      return words[i]; 
+       
+  return word;
+}
+
+int main() {
+  char phrase[MAX];
+  fgets(phrase, MAX, stdin);
+  
+  for (char *word = strtok(phrase, " "); word; word = strtok(NULL, " ")) 
+   	 printf("%s ", numberToWord(word));
+  
+  return 0;
+}
