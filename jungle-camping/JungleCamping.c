df1dec940dfa34a414c3d337608e471da3629082
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-04 16:54:03 +0200

    Update JungleCamping.c

--- jungle-camping/JungleCamping.c
+++ jungle-camping/JungleCamping.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#define MAX 500
 
 int main() {
   char *animals[] = {"Lion", "Tiger", "Snake", "Bird"};
   char *sounds[] = {"Grr", "Rawr", "Ssss", "Chirp"};
    
-  char noise[500];
-  fgets(noise, 500, stdin);
+  char noise[MAX];
+  fgets(noise, MAX, stdin);
   
   for (char *sound = strtok(noise, " "); sound; sound = strtok(NULL, " ")) 
     for (int i = 0; i < 4; ++i)
