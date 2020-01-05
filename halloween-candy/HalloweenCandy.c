dd607c361ef33f1544b87c4f2a80a3b94084d400
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-05 08:44:02 +0200

    Update HalloweenCandy.c

--- halloween-candy/HalloweenCandy.c
+++ halloween-candy/HalloweenCandy.c
@@ -5,7 +5,7 @@ int main() {
     int houses;
     scanf("%d", &houses);
 
-    int percentage = (int)ceil(100 * 2.0 / houses);
+    int const percentage = (int)ceil(100 * 2.0 / houses);
     printf("%d", percentage);
     
     return 0;
