6ce79cbc9c3713bbb608a025c14607c7173b9136
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-05 08:40:55 +0200

    Update ExtraTerrestrials.c

--- extra-terristrials/ExtraTerrestrials.c
+++ extra-terristrials/ExtraTerrestrials.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#define MAX 80
 
 int main() {
-  char word[80];
-  fgets(word, 80, stdin);
+    char word[MAX];
+    int i = strlen(fgets(word, MAX, stdin));
 	
-  int i = strlen(word);
-  while (i--) printf("%c", word[i]);
+    while (i--) putchar(word[i]);
 	  
-  return 0;
+    return 0;
 }
