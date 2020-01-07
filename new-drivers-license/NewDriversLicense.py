673a9e6528390b4e4146bf3775909058e3f11524
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-07 23:46:29 +0200

    Create NewDriversLicense.py

---
+++ new-drivers-license/NewDriversLicense.py
@@ -0,0 +1,12 @@
+name = input()
+agents = int(input())
+persons = input().split()
+	
+position = 0;
+
+for person in persons:
+    if person < name:
+        position += 1
+        
+time = 20 * (1 + position // agents)
+print(time)
