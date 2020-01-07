df26cc49620cb2714f3962a63fbdec5583ec7d0e
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-07 18:12:35 +0200

    Update Security.c

--- security/Security.c
+++ security/Security.c
@@ -3,10 +3,11 @@
 
 int main() {
   char floor[MAX];
-  char *message;
+  char *message = NULL;
   int status = 0;
+  fgets(floor, MAX, stdin);
   
-  for (char *c = fgets(floor, MAX, stdin); *c && !message; ++c) 
+  for (char *c = floor; *c && !message; ++c) 
     if (*c == 'G' && status) message = "quiet";
     else if ((*c == '$' || *c == 'T') && status++) message = "ALARM"; 
   	   
