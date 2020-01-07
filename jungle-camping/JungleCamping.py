1e85992ac602b132d9cc8aa426526d68b7b5acc8
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-07 23:09:56 +0200

    Update JungleCamping.py

--- jungle-camping/JungleCamping.py
+++ jungle-camping/JungleCamping.py
@@ -1,9 +1,6 @@
+noise = input().split()
+
 animals = {"Grr": "Lion", "Rawr": "Tiger", "Ssss": "Snake", "Chirp": "Bird"}
 
-noise = input().split()
-output = ""
-
 for sound in noise:
-    output += animals[sound] + " "
-    
-print(output)
+    print(animals[sound], end = ' ')
