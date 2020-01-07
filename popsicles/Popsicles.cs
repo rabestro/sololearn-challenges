5d234c33e149e87816862461889f87c364c013bb
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-07 23:37:16 +0200

    Create Popsicles.cs

---
+++ popsicles/Popsicles.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int siblings, popsicles;
+            siblings = Convert.ToInt32(Console.ReadLine());
+            popsicles = Convert.ToInt32(Console.ReadLine());
+            
+            //your code goes here
+            
+            bool isEven = popsicles % siblings == 0;
+            
+            if (isEven) {
+            	   Console.WriteLine("give away");
+            }
+            else {
+            	   Console.WriteLine("eat them yourself");
+            }
+        }
+    }
+}
