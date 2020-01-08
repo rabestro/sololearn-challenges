15288deec9c1b96e43ba876e21ac48735204b2eb
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 00:16:24 +0200

    Update PasswordValidation.java

--- password-validation/PasswordValidation.java
+++ password-validation/PasswordValidation.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Program
+public class PasswordValidation
 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String password = input.nextLine();
+        var input = new Scanner(System.in);
+        var password = input.nextLine();
 	    
         boolean isStrong = password.length() > 6
            && Pattern.matches(".*\\d.*\\d.*", password)
