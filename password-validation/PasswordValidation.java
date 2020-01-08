9534816750c4931edf88fad8d3df2faf1119d4d5
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-08 23:43:29 +0200

    Update PasswordValidation.java

--- password-validation/PasswordValidation.java
+++ password-validation/PasswordValidation.java
@@ -3,15 +3,14 @@ import java.util.regex.Pattern;
 
 public class Program
 {
-  public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
-      String password = input.nextLine();
-      boolean isStrong = false;
-      validation: {
-          if (password.length() < 7) break validation;
-          if (!Pattern.matches(".*\\d.*\\d.*", password)) break validation;
-          isStrong = Pattern.matches("(.*[!@#$%&*].*){2,}", password);
-      }
-      System.out.println(isStrong ? "Strong" : "Weak");
-  }
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    String password = input.nextLine();
+	    
+	    boolean isStrong = password.length() > 6
+	        && Pattern.matches(".*\\d.*\\d.*", password)) 
+	        && Pattern.matches("(.*[!@#$%&*].*){2,}", password);
+	        
+       System.out.println(isStrong ? "Strong" : "Weak");
+	}
 }
