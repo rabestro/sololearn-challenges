2455e6fc60dcb04efd8d9bedb6d8f032bb8ca1f6
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 00:47:31 +0200

    Update Security.java

--- security/Security.java
+++ security/Security.java
@@ -4,10 +4,9 @@ import java.util.regex.Pattern;
 public class Security
 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String floor = input.nextLine();
-	      
-        boolean isSafe = Pattern.matches(".*[$T].*G.*[$T].*", floor);
+        var input = new Scanner(System.in);
+        var floor = input.nextLine();      
+        var isSafe = Pattern.matches(".*[$T].*G.*[$T].*", floor);
 	      
         System.out.println(isSafe ? "quiet" : "ALARM");
     }
