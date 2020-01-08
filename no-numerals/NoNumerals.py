a7e3857826aa1014de84c0841cf00f4fb39f9fcd
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 00:52:56 +0200

    Update NoNumerals.py

--- no-numerals/NoNumerals.py
+++ no-numerals/NoNumerals.py
@@ -1,11 +1,9 @@
 phrase = input().split()
-
-numbers = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
-	   6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten'}
-			 
 output = ''
+numbers = 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'
+
 for word in phrase:
-    if word.isdigit() and int(word) in numbers:
+    if word.isdigit() and int(word) in range(11):
         output += numbers[int(word)] + ' '
     else:
         output += word + ' '
