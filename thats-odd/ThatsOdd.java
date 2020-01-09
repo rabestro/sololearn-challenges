5888c455f4ad1d4bf361cda1bea50276943eda39
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 21:24:27 +0200

    Update ThatsOdd.java

--- thats-odd/ThatsOdd.java
+++ thats-odd/ThatsOdd.java
@@ -10,7 +10,7 @@ public class Program
         while (count-- > 0) {
             final int number = input.nextInt();
             if (number % 2 == 0) {
-            	   summa += number;
+            	summa += number;
             }
         }
         System.out.print(summa);
