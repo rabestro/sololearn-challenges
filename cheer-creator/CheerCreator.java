32a7909c2a30458fdabf93e2d19cf758df515b84
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 19:37:36 +0200

    Update CheerCreator.java

--- cheer-creator/CheerCreator.java
+++ cheer-creator/CheerCreator.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class CheerCreator
 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int yards = input.nextInt();
+        var input = new Scanner(System.in);
+        var yards = input.nextInt();
 
-        if (yards > 10) 
-            System.out.print("High Five");
-        else if (yards < 1) 
-            System.out.print("shh");
-        else while (yards-- > 0) 
-            System.out.print("Ra!");
+        if (yards > 10) {
+        	  System.out.print("High Five");
+        } else if (yards < 1) {
+        	  System.out.print("shh");
+        } else {
+        	  while (yards-- > 0) {
+        	  	   System.out.print("Ra!");
+        	  }
+        }
     }
 }
