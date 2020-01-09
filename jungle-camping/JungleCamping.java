b9d94a24481eb208fbf11313b13ce4148fac533d
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 21:11:32 +0200

    Update JungleCamping.java

--- jungle-camping/JungleCamping.java
+++ jungle-camping/JungleCamping.java
@@ -5,7 +5,7 @@ public class JungleCamping
 {
     public static void main(String[] args) {
         Map<String, String> animals = Map.of(
-            "Grr","Lion", "Rawr", "Tiger", "Ssss", "Snake", "Chirp", "Bird");
+            "Grr", "Lion", "Rawr", "Tiger", "Ssss", "Snake", "Chirp", "Bird");
 	    
         final var input = new Scanner(System.in);
         final var noise = input.nextLine().split(" ");
