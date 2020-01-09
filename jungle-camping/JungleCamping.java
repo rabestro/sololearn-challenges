e5c2dd77484dad3d12d58fcc15b3efaf3a2f6700
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 20:48:14 +0200

    Create JungleCamping.java

---
+++ jungle-camping/JungleCamping.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Map;
+
+public class JungleCamping
+{
+    public static void main(String[] args) {
+        Map<String, String> animals = Map.of(
+            "Grr","Lion", "Rawr", "Tiger", "Ssss", "Snake", "Chirp", "Bird");
+	    
+        final var input = new Scanner(System.in);
+        final var noise = input.nextLine().split(" ");
+        
+        for (var sound : noise) {
+        	  System.out.printf("%s ", animals.get(sound));
+        }
+    }
+}
