9cbdd885330e1d6af9bf9ea109068a635517d8ca
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 19:28:18 +0200

    Create FruitBowl.java

---
+++ fruit-bowl/FruitBowl.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FruitBowl
+{
+    public static void main(String[] args) {
+        final Scanner input = new Scanner(System.in);
+        final int fruit = input.nextInt();
+        
+        //your code goes here
+        
+        final int apples = fruit / 2;
+        final int pies = apples / 3; 
+        
+        System.out.print(pies);
+    }
+}
