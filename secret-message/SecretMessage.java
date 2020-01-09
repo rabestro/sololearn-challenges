6fb62f60af7f3355df74c3cc1bd8df2e8c366f99
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 23:23:53 +0200

    Create SecretMessage.java

---
+++ secret-message/SecretMessage.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SecretMessage
+{
+    public static void main(String[] args) {
+        final var input = new Scanner(System.in);
+        final var message = input.nextLine();
+        
+        message.toLowerCase().chars().forEach(i -> {
+            char encoded = (char)(i >= 'a' && i <= 'z' ? 'a' + 'z' - i : i);
+            System.out.print(encoded); });
+    }
+}
