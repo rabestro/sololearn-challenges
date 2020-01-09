15d56e78f90096c60bdd975d621a808fc1768b7f
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 19:30:22 +0200

    Update FruitBowl.c

--- fruit-bowl/FruitBowl.c
+++ fruit-bowl/FruitBowl.c
@@ -4,7 +4,12 @@ int main() {
     int fruit;
     scanf("%d", &fruit);
 
-    int const pies = fruit / 6;
+    //your code goes here
+    
+    int const apples = fruit / 2;
+    int const pies = apples / 3;
+    
     printf("%d", pies);
+    
     return 0;
 }
