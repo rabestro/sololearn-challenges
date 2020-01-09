ce14f73f5e4226fdc8cdc7e1af2dcdc704d01e4e
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-09 21:21:17 +0200

    Update TheSpyLife.java

--- the-spy-life/TheSpyLife.java
+++ the-spy-life/TheSpyLife.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class TheSpyLife
 {
     public static void main(String[] args) {
-        var input = new Scanner(System.in);
-        var encoded = new StringBuilder(input.nextLine());
-        var message = encoded.reverse().toString().replaceAll("[^a-zA-Z ]", "");
+        final var input = new Scanner(System.in);
+        final var encoded = new StringBuilder(input.nextLine());
+        final var message = encoded.reverse().toString().replaceAll("[^a-zA-Z ]", "");
         
         System.out.print(message);
     }
