898ea879fc5f2aa071b2f64bf1009b8232291eb0
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-10 01:36:55 +0200

    Create ConvertDate.java

---
+++ convert-date/ConvertDate.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ConvertDate
+{
+    public static void main(String[] args) {
+        final var input = new Scanner(System.in);
+
+        input.findInLine("(\\d{2}|\\w)[ /](\\d\\d)[,/](\\d{4})");
+        final var usDate = input.match();
+        
+        final int month = 
+            switch (usDate.group(1)) {
+           	   case "January" -> 1;
+           	   case "February" -> 2;
+           	   case "March" -> 3;
+           	   case "April" -> 4;
+           	   case "May" -> 5;
+           	   case "June" -> 6;
+           	   case "July" -> 7;
+           	   case "August" -> 8;
+           	   case "September" -> 9;
+           	   case "October" -> 10;
+           	   case "November" -> 11;
+           	   case "December" -> 12;
+           	   default -> Integer.valueOf(usDate.group(1));
+            };
+        final var day = usDate.group(2);
+        final var year = usDate.group(3);
+        
+        System.out.printf("%d/%s/%s", day, month, year);
+    }
+}
