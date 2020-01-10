1c15d27c02e11e260c5872ee182b92b47c4187fb
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-10 02:38:12 +0200

    Update HalloweenCandy.java

--- halloween-candy/HalloweenCandy.java
+++ halloween-candy/HalloweenCandy.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
-import java.lang.Math;
 
 public class HalloweenCandy
 {
     public static void main(String[] args) {
         final Scanner input = new Scanner(System.in);
         final int houses = input.nextInt();
-        final int percentage = (int) Math.ceil(200. / houses);
+        final int percentage = (int) java.lang.Math.ceil(200. / houses);
        
         System.out.print(percentage);
     }
