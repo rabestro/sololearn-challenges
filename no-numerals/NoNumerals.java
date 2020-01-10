e79d99efb2adc4d25dbe348fd07eb35ea6aa7528
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-10 22:17:51 +0200

    Create NoNumerals.java

---
+++ no-numerals/NoNumerals.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NoNumerals
+{
+    public static void main(String[] args) {
+        final var input = new Scanner(System.in);
+        final var phrase = input.nextLine();
+        final String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        final var updatedPhrase = new StringBuilder();
+        
+        for (var word : phrase.split(" ")) {
+            try {
+                int number = Integer.parseInt(word);
+                if (number >= 0 && number <= 10) {
+                	    updatedPhrase.append(numbers[number]);
+                } else {
+                	    updatedPhrase.append(word);
+                }
+            } catch (NumberFormatException nfe) {
+                updatedPhrase.append(word);
+            }
+            updatedPhrase.append(" ");
+        }
+        System.out.print(updatedPhrase);
+    }
+}
