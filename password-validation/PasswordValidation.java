b4c054d5147f1cc88caadf86c644e1d2741bd7d6
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-10 02:06:01 +0200

    Update PasswordValidation.java

--- password-validation/PasswordValidation.java
+++ password-validation/PasswordValidation.java
@@ -4,10 +4,10 @@ import java.util.regex.Pattern;
 public class PasswordValidation
 {
     public static void main(String[] args) {
-        var input = new Scanner(System.in);
-        var password = input.nextLine();
+        final var input = new Scanner(System.in);
+        final var password = input.nextLine();
 	    
-        boolean isStrong = password.length() > 6
+        final var isStrong = password.length() > 6
            && Pattern.matches(".*\\d.*\\d.*", password)
            && Pattern.matches("(.*[!@#$%&*].*){2,}", password);
 	        
