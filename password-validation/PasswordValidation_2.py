5d17deaee6fa5a6fbac1390f3480f09c9b072aac
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-11 19:04:32 +0200

    Create PasswordValidation_2.py

---
+++ password-validation/PasswordValidation_2.py
@@ -0,0 +1,11 @@
+import re
+password = input()
+
+isStrong = len(password) > 6 \
+    and re.search(r'\d.*\d', password) \
+    and re.search(r'([!@#$%&*].*){2,}', password)
+      
+if isStrong:
+  print("Strong")
+else:
+  print("Weak")
