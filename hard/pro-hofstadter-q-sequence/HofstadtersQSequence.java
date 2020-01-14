6b91a4e602451e9e22be7c98f9b80481b1fa65e5
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-14 20:01:34 +0200

    Update HofstadtersQSequence.java

--- hard/pro-hofstadter-q-sequence/HofstadtersQSequence.java
+++ hard/pro-hofstadter-q-sequence/HofstadtersQSequence.java
@@ -1,13 +1,13 @@
 public class HofstadtersQSequence
 {
     public final static int Q(int n) {
-        int[] q = new int[n + 1];
+        int[] q = new int[n];
+        q[0] = 1;
         q[1] = 1;
-        q[2] = 1;
-        for (int i = 3; i <= n; ++i) {
+        for (int i = 2; i < n; ++i) {
             q[i] = q[i - q[i - 1]] + q[i - q[i - 2]];
         }
-        return q[n];
+        return q[n - 1];
     }
     
     public static void main(String[] args) {
