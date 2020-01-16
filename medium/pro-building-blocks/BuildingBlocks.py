a0e87ebdb3355467ba193082f359a19933b13598
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-17 01:58:22 +0200

    Update BuildingBlocks.py

--- medium/pro-building-blocks/BuildingBlocks.py
+++ medium/pro-building-blocks/BuildingBlocks.py
@@ -1,4 +1,5 @@
 STUDENTS = 15
-blocks = [int(input()) for i in range(4)]
+COLORS = 4
+blocks = [int(input()) for i in range(COLORS)]
 left_over = sum(map(lambda x: x % STUDENTS, blocks))
 print(left_over)
