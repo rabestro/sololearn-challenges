203cd6c0fe216d653adb5ccdaabbc10925160de9
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-17 08:42:13 +0200

    Create Map2D.java

---
+++ hard/pro-2d-map/Map2D.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Map2D
+{
+    public static int SIZE = 5 + 1; // Size of square + comma
+    
+    public static void main(String[] args) {
+        final var input = new Scanner(System.in);
+        final var map = input.nextLine();
+        input.close();
+        
+        final int indexA = map.indexOf('P');
+        final int indexB = map.indexOf('P', indexA + 1);
+        
+        final int path = indexB / SIZE - indexA / SIZE + Math.abs(indexA % SIZE - indexB % SIZE);
+        
+        System.out.print(path);
+    }
+}
