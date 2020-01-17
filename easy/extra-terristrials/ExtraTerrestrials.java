d16bb33772d1afbd509c997ab7ba0c9dc22d96a6
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-17 08:53:05 +0200

    Update ExtraTerrestrials.java

--- easy/extra-terristrials/ExtraTerrestrials.java
+++ easy/extra-terristrials/ExtraTerrestrials.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class ExtraTerrestrials
 {
     public static void main(String[] args) {
-        var input = new Scanner(System.in);
-        var word = new StringBuilder(input.nextLine());
+        final var input = new Scanner(System.in);
+        final var word = new StringBuilder(input.nextLine());
+        input.close();
         
         System.out.print(word.reverse());
     }
