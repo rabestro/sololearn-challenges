715c0d8f87ad09b20599728235e67a9189ec1707
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-17 18:46:58 +0200

    Update Security.java

--- hard/security/Security.java
+++ hard/security/Security.java
@@ -3,11 +3,16 @@ import java.util.regex.Pattern;
 
 public class Security
 {
+    final static String GUARD_BETWEEN_THIEF_AND_MONEY = ".*[$T].*G.*[$T].*";
+    
     public static void main(String[] args) {
-        var input = new Scanner(System.in);
-        var floor = input.nextLine();      
-        var isSafe = Pattern.matches(".*[$T].*G.*[$T].*", floor);
-	      
-        System.out.println(isSafe ? "quiet" : "ALARM");
-    }
+	    final var input = new Scanner(System.in);
+	    final var floor = input.nextLine();
+	    input.close();
+	    
+	    boolean isSafe = Pattern.matches(GUARD_BETWEEN_THIEF_AND_MONEY, floor);
+	        
+       System.out.println(isSafe ? "quiet" : "ALARM");
+	}
 }
+
