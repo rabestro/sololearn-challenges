30ee52686bc61769f3f3238d6648145ffddd4530
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-17 18:48:13 +0200

    Update Security.java

--- hard/security/Security.java
+++ hard/security/Security.java
@@ -6,13 +6,12 @@ public class Security
     final static String GUARD_BETWEEN_THIEF_AND_MONEY = ".*[$T].*G.*[$T].*";
     
     public static void main(String[] args) {
-	    final var input = new Scanner(System.in);
-	    final var floor = input.nextLine();
-	    input.close();
+        final var input = new Scanner(System.in);
+        final var floor = input.nextLine();
+        input.close();
 	    
-	    boolean isSafe = Pattern.matches(GUARD_BETWEEN_THIEF_AND_MONEY, floor);
+        boolean isSafe = Pattern.matches(GUARD_BETWEEN_THIEF_AND_MONEY, floor);
 	        
-       System.out.println(isSafe ? "quiet" : "ALARM");
-	}
+        System.out.println(isSafe ? "quiet" : "ALARM");
+    }
 }
-
