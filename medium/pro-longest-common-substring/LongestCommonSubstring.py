016341b9f05588898558cb840e30fbadec277599
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-19 01:53:22 +0200

    Update LongestCommonSubstring.py

--- medium/pro-longest-common-substring/LongestCommonSubstring.py
+++ medium/pro-longest-common-substring/LongestCommonSubstring.py
@@ -5,7 +5,7 @@ length = len(word)
 
 for i in range(length, 0, -1):
     substrings = set(word[j:j + i] for j in range(0, 1 + length - i))
-    ok = sorted(list(filter(lambda s: all(map(lambda x: s in x, words[1:])), substrings)))
+    ok = sorted(list(filter(lambda ss: all(map(lambda w: ss in w, words[1:])), substrings)))
     if ok: break
 
 print(ok[0])
