d11aa2636d637e886eba40d34d4756cf19c44a52
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-19 02:14:58 +0200

    Update Security.py

--- hard/security/Security.py
+++ hard/security/Security.py
@@ -1,8 +1,4 @@
 import re
 floor = input()
-isSafe = re.search("[$T].*G.*[$T]", floor)
-
-if isSafe: 
-    print('quiet')
-else: 
-    print('ALARM')
+alarm = re.search("[$T].*G.*[$T]", floor) is None
+print(('quiet','ALARM')[alarm])
