fdf9029d78c9867f54ec7a59ec6a587172238764
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-20 00:26:28 +0200

    Create MilitaryTime.java

---
+++ medium/military-time/MilitaryTime.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Locale;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class MilitaryTime
+{
+    public static void main(String[] args) {
+        final var input = new Scanner(System.in);
+        final var time = LocalTime.parse(
+            input.nextLine(), 
+            DateTimeFormatter
+                .ofLocalizedTime(FormatStyle.SHORT)
+                .withLocale(Locale.US));
+        
+        System.out.println(time);
+    }
+}
