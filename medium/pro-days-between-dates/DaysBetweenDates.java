b5cb75091d12cb2c68a70297eb8556de72bdd968
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-20 02:15:37 +0200

    Create DaysBetweenDates.java

---
+++ medium/pro-days-between-dates/DaysBetweenDates.java
@@ -0,0 +1,25 @@
+# Days between dates  
+
+You need to calculate exactly how many days have passed between two dates.
+
+## Task:  
+Calculate how many days have passed between two input dates, and output the result. 
+
+## Input Format: 
+Two strings that represent the dates, first date should be the older date. 
+Date format: Month DD, YYYY
+
+## Output Format: 
+A number representing the number of days between the two dates.
+
+## Sample Input: 
+```
+August 15, 1979
+June 15, 2018
+```
+## Sample Output: 
+```
+14184
+```
+## Explanation: 
+14184 days have passed between the two input days.
