c3483ef3111af390b25a954b4e1b2419179484ea
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-22 19:45:01 +0200

    Update JungleCamping.java

--- easy/jungle-camping/JungleCamping.java
+++ easy/jungle-camping/JungleCamping.java
@@ -5,11 +5,11 @@ import java.util.stream.Stream;
 public class JungleCamping
 {
     public static void main(String[] args) {
-	    final var in = new Scanner(System.in);
-	    final Map<String, String> animals = Map.of(
+        final var in = new Scanner(System.in);
+	final Map<String, String> animals = Map.of(
             "Grr", "Lion ", "Rawr", "Tiger ", "Ssss", "Snake ", "Chirp", "Bird ");
             
-	    Stream.generate(in::next)
+        Stream.generate(in::next)
             .map(animals::get)
             .forEach(System.out::print);
     }
