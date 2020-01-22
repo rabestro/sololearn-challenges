8767b18fb4ba20eb7062fcd0bcd9d8efdc669827
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-22 21:44:47 +0200

    Update Symbols.java

--- medium/symbols/Symbols.java
+++ medium/symbols/Symbols.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class Symbols
 {
-    public static void main(final String[] args) {
-        final var input = new Scanner(System.in);
-        final var text = input.nextLine();
-        input.close();
+    public static void main(String[] args) {
         
-        final var message = text.replaceAll("[^a-zA-Z0-9 ]", "");
+        var message = new Scanner(System.in)
+            .nextLine()
+            .replaceAll("[^a-zA-Z0-9 ]", "");
         
         System.out.print(message);
     }
