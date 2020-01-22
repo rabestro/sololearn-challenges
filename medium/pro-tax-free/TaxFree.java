b64cdd0fc33bd366d0e6452095b78fecad17cec2
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-23 01:24:20 +0200

    Create TaxFree.java

---
+++ medium/pro-tax-free/TaxFree.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TaxFree
+{
+    public static void main(String[] args) {
+            
+        double sum = new Scanner(System.in)
+            .useDelimiter(",")
+            .tokens()
+            .mapToInt(Integer::parseInt)
+            .mapToDouble(i -> i < 20 ? i * 1.07 : i)
+            .sum();
+            
+        System.out.printf("%.2f", sum);
+    }
+}
