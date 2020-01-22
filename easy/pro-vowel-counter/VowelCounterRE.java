c50ebbdd3393c0b63461327cb2ec22150a69aa04
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-22 21:35:20 +0200

    Update VowelCounterRE.java

--- easy/pro-vowel-counter/VowelCounterRE.java
+++ easy/pro-vowel-counter/VowelCounterRE.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class VowelCounterRE
 {
     public static void main(String[] args) {
-        final var input = new Scanner(System.in);
-        final var sentence = input.nextLine();
-        input.close();
         
-        final var vowels = sentence.replaceAll("[^AEIOUaeiou]", "").length();
+        long vowels = new Scanner(System.in)
+            .nextLine()
+            .replaceAll("[^AEIOUaeiou]", "")
+            .length();
         
         System.out.println(vowels);
     }
