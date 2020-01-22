dcc1a1300824cd2ff145a0fb5ad469958e769d2d
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-22 20:40:16 +0200

    Update PigLatin.java

--- medium/pig-latin/PigLatin.java
+++ medium/pig-latin/PigLatin.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class PigLatin {
     public static void main(String[] args) {
-        final var input = new Scanner(System.in);
-        
-        Stream.generate(input::next)
+    
+        new Scanner(System.in).tokens()
             .map(word -> word.substring(1) + word.charAt(0) + "ay ")
             .forEach(System.out::print);
     }
