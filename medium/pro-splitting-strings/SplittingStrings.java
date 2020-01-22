483d56e5c963020836ac4e214e0376cd4b69cb7b
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-23 01:07:41 +0200

    Create SplittingStrings.java

---
+++ medium/pro-splitting-strings/SplittingStrings.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
+public class SplittingStrings
+{
+    public static void main(String[] args) {
+        final var input = new Scanner(System.in);
+        final var text = input.nextLine();
+        final var n = input.nextInt();
+        final var len = text.length();
+        
+        var separated = IntStream
+            .range(0, (len % n > 0 ? 1 : 0) + len / n)
+            .mapToObj(i -> text.substring(i * n, Math.min(n + i * n, len)))
+            .collect(Collectors.joining("-"));
+            
+        System.out.print(separated);
+   }
+}
