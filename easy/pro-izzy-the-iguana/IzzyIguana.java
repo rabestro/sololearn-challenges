7111a9b28837a92182180433fe4d337a61ce6438
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-22 21:10:59 +0200

    Create IzzyIguana.java

---
+++ easy/pro-izzy-the-iguana/IzzyIguana.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+import java.util.Scanner;
+
+public class IzzyIguana
+{
+    public static void main(String[] args) {
+	    
+        final Map<String, Integer> snackPoints = Map.of(
+            "Lettuce", 5, "Carrot", 4, "Mango", 9, "Cheeseburger", 0);
+            
+        int sum = new Scanner(System.in).tokens()
+            .mapToInt(snackPoints::get)
+            .sum();
+            
+        System.out.println(sum >= 10 ? "Come on Down!" : "Time to wait");
+    }
+}
