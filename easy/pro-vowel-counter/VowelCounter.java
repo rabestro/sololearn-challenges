362d579ad6ab2ffcf1aec6cc6b0fef7ce1791d18
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-22 21:29:42 +0200

    Create VowelCounter.java

---
+++ easy/pro-vowel-counter/VowelCounter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VowelCounter
+{
+    public static void main(String[] args) {
+        
+        long vowels = new Scanner(System.in)
+            .nextLine()
+            .chars()
+            .filter(c -> "AEIOUaeiou".indexOf(c) != -1)
+            .count();
+        
+        System.out.println(vowels);
+    }
+}
