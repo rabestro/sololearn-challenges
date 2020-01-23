63704df205493e5f74dad478a5d536ceec91286d
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-23 14:32:25 +0200

    Update ConvertDate.java

--- medium/convert-date/ConvertDate.java
+++ medium/convert-date/ConvertDate.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.util.Locale;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -7,14 +6,15 @@ import java.time.format.FormatStyle;
 public class ConvertDate {
 
     public static void main(String[] args) {
-        final var input = new Scanner(System.in);
-        final var usDate = input.nextLine();
-        input.close();
         
-        final var euDate = DateTimeFormatter.ofPattern("d/M/yyyy").format(
-            LocalDate.parse(usDate, Character.isDigit(usDate.charAt(0))
+        final var usDate = new java.util.Scanner(System.in).nextLine();
+        
+        final var usFormat = Character.isDigit(usDate.charAt(0))
                 ? DateTimeFormatter.ofPattern("M/d/yyyy")
-                : DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).withLocale(Locale.US)));
+                : DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).withLocale(Locale.US)
+        
+        final var euDate = DateTimeFormatter
+            .ofPattern("d/M/yyyy").format(LocalDate.parse(usDate, usFormat));
         
         System.out.println(euDate);
     }
