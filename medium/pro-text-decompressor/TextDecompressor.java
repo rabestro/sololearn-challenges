c72a1a283432a6f77cf8b5a80ed836a5b4d2b7f6
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-23 02:09:38 +0200

    Create TextDecompressor.java

---
+++ medium/pro-text-decompressor/TextDecompressor.java
@@ -0,0 +1,11 @@
+public class TextDecompressor
+{
+    public static void main(String[] args) {
+        
+        new java.util.Scanner(System.in)
+            .useDelimiter("(?<=\\d)")
+            .tokens()
+            .map(x -> x.substring(0,1).repeat(Integer.parseInt(x.substring(1))))
+            .forEach(System.out::print);
+    }
+}
