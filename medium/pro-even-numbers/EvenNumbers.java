66e723ec315fcbfd653e7192c4b9e2810611940e
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-23 16:31:13 +0200

    Update EvenNumbers.java

--- medium/pro-even-numbers/EvenNumbers.java
+++ medium/pro-even-numbers/EvenNumbers.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
-import java.util.stream.IntStream;
-
 public class EvenNumbers
 { 
     public static void main(String[] args) {
-        final var in = new Scanner(System.in);
+        final var in = new java.util.Scanner(System.in);
         
-        IntStream
+        java.util.stream.IntStream
             .generate(in::nextInt)
             .filter(i -> i % 2 == 0)
             .forEach(i -> System.out.printf("%d ", i));
