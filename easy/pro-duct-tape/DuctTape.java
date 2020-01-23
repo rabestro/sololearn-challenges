f828ad617cab76bd226807dd4eea1a802cc2f7e1
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-23 16:19:08 +0200

    Update DuctTape.java

--- easy/pro-duct-tape/DuctTape.java
+++ easy/pro-duct-tape/DuctTape.java
@@ -4,7 +4,7 @@ public class DuctTape
 	    final var in = new java.util.Scanner(System.in);
 	    final var doorHeight = in.nextInt();
 	    final var doorWidth = in.nextInt();
-			in.close();
+            in.close();
 			
 	    final var rollSquare = 10; // 60ft long * 2" / 12" width
 	    final var doorSquare = 2 * doorHeight * doorWidth; // Both sides of the door!
