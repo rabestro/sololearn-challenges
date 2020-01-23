6eb40cfdc82ab01b8bd48a9c85329babaac0d6ff
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-23 02:14:02 +0200

    Update TextDecompressor.java

--- medium/pro-text-decompressor/TextDecompressor.java
+++ medium/pro-text-decompressor/TextDecompressor.java
@@ -1,7 +1,7 @@
 public class TextDecompressor
 {
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
+        
         new java.util.Scanner(System.in)
             .useDelimiter("(?<=\\d)")
             .tokens()
