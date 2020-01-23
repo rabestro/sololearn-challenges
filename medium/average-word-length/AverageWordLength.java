9623daa593c61e05b9555f1e3c33b08891a98e39
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-23 22:39:19 +0200

    Update AverageWordLength.java

--- medium/average-word-length/AverageWordLength.java
+++ medium/average-word-length/AverageWordLength.java
@@ -6,7 +6,7 @@ public class AverageWordLength
         final var input = new Scanner(System.in);
         final var essay = input.nextLine();
         final int words = essay.split(" ").length;
-        final int chars = essay.replaceAll("[^a-zA-Z]", "").length();
+        final int chars = essay.replaceAll("\\W", "").length();
         
         final int average = (int) java.lang.Math.ceil((double) chars / words);
         
