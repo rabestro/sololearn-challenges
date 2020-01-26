e425e313bc384049c6fd8128c4a80896a108d683
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-27 00:26:16 +0200

    Create PigLatin.java

---
+++ one-line-java/PigLatin.java
@@ -0,0 +1,9 @@
+public class PigLatin {
+    public static void main(String[] args) {
+    
+        new java.util.Scanner(System.in)
+            .tokens()
+            .map(word -> word.substring(1) + word.charAt(0) + "ay ")
+            .forEach(System.out::print);
+    }
+}
