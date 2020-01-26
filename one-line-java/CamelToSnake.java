bb54ee5281429d60ba4ba69a1762114fd4313d87
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-27 00:21:00 +0200

    Create CamelToSnake.java
    Issue #1

---
+++ one-line-java/CamelToSnake.java
@@ -0,0 +1,10 @@
+public class CamelToSnake {
+    public static void main(String[] args) {
+    
+        System.out.println(
+            new java.util.Scanner(System.in)
+                .nextLine()
+                .replaceAll("(?<=[^//b^])([A-Z])", "_$1")
+                .toLowerCase());
+    }
+}
