df3db425ee5486137c1e83c3582220a10fec1f6f
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-27 00:36:06 +0200

    Create SnapCrackleAndPop.java

---
+++ one-line-java/SnapCrackleAndPop.java
@@ -0,0 +1,10 @@
+public class SnapCrackleAndPop {
+    public static void main(String[] args) {
+    
+        new java.util.Scanner(System.in)
+            .tokens()
+            .mapToInt(Integer::parseInt)
+            .mapToObj(i -> i % 3 == 0 ? "Pop " : i % 2 == 0 ? "Crackle " : "Snap ")
+            .forEach(System.out::print);
+    }
+}
