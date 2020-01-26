47c5d2022cffb578c9cbe1c4031bebde9ca688ec
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-27 01:21:05 +0200

    Create EvenNumbers.java

---
+++ one-line-java/EvenNumbers.java
@@ -0,0 +1,10 @@
+public class EvenNumbers { 
+    public static void main(String[] args) {
+        
+        new java.util.Scanner(System.in)
+            .tokens()
+            .mapToInt(Integer::parseInt)
+            .filter(i -> i % 2 == 0)
+            .forEach(i -> System.out.printf("%d ", i));
+    }
+}
