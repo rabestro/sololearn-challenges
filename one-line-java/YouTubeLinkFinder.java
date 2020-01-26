5177be6c88fd64d1bac39b8826e44a87c9cd3861
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-27 01:49:02 +0200

    Create YouTubeLinkFinder.java

---
+++ one-line-java/YouTubeLinkFinder.java
@@ -0,0 +1,9 @@
+public class YouTubeLinkFinder {
+    public static void main(String[] args) {
+        
+        System.out.print(
+            new java.util.Scanner(System.in)
+                .nextLine()
+                .replaceAll(".*[=/](.*)$", "$1"));
+    }
+}
