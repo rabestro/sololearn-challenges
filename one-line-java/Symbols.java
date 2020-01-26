4132b54a2bfdb76dac7e21ccbbdcd74ffee02c13
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-27 01:09:01 +0200

    Create Symbols.java

---
+++ one-line-java/Symbols.java
@@ -0,0 +1,9 @@
+public class Symbols {
+    public static void main(String[] args) {
+        
+        System.out.print(
+            new java.util.Scanner(System.in)
+                .nextLine()
+                .replaceAll("[^a-zA-Z0-9 ]", ""));
+    }
+}
