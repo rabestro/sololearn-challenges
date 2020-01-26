dabad21e71449fdd6f32c895d29e37b0191b13db
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-27 01:15:18 +0200

    Create NoNumerals.java

---
+++ one-line-java/NoNumerals.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+public class NoNumerals {
+    static final Map<String, String> numbers = Map.of(
+            "10", "ten", "1", "one", "2", "two", "3", "three", "4", "four", 
+            "5", "five", "6", "six", "7", "seven", "8", "eight", "9", "nine");
+            
+    public static void main(String[] args) {
+	    
+        new java.util.Scanner(System.in)
+            .tokens()
+            .map(word -> (numbers.containsKey(word) ? numbers.get(word) : word) + " ")
+            .forEach(System.out::print);
+    }
+}
