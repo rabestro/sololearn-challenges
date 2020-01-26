1a215731bd6ff5fdacf34e92c30cce3fcbc6126a
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-27 01:31:39 +0200

    Create TaxFree.java

---
+++ one-line-java/TaxFree.java
@@ -0,0 +1,12 @@
+public class TaxFree {
+    public static void main(String[] args) {
+           
+        System.out.printf("%.2f", 
+            new java.util.Scanner(System.in)
+                .useDelimiter(",")
+                .tokens()
+                .mapToInt(Integer::parseInt)
+                .mapToDouble(i -> i < 20 ? i * 1.07 : i)
+                .sum(););
+    }
+}
