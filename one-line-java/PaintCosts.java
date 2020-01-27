4b2095e46c96d6cd677f958e141ec2259a142f55
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-27 14:44:55 +0200

    Create PaintCosts.java

---
+++ one-line-java/PaintCosts.java
@@ -0,0 +1,6 @@
+public class PaintCosts {
+    public static void main(String[] args) {
+        
+        System.out.print((int)Math.round(1.1 * (5 * new java.util.Scanner(System.in).nextInt() + 40)));
+    }
+}
