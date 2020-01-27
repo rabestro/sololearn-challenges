b1b3334094e242990bdd1423e772c7ad48fcbde1
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-27 14:20:32 +0200

    Create HalloweenCandy.java

---
+++ one-line-java/HalloweenCandy.java
@@ -0,0 +1,6 @@
+public class HalloweenCandy {
+    public static void main(String[] args) {
+        
+        System.out.print((int)Math.ceil(200. / new java.util.Scanner(System.in).nextInt()));
+    }
+}
