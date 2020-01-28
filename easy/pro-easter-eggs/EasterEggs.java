d604314e37914d696907e0793bd7759f40d73528
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-28 16:06:36 +0200

    Create EasterEggs.java

---
+++ easy/pro-easter-eggs/EasterEggs.java
@@ -0,0 +1,15 @@
+public class EasterEggs {
+
+    public static void main(final String[] args) {
+    
+        final var input = new java.util.Scanner(System.in);
+        final var totalNumberEggs = input.nextInt();
+        final var yourAmount = input.nextInt();
+        final var friendsAmount = input.nextInt();
+        input.close();
+        
+        final var isStillEggs = totalNumberEggs > yourAmount + friendsAmount;
+        
+        System.out.print(isStillEggs ? "Keep Hunting" : "Candy Time");
+    }
+}
