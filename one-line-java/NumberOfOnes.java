23af0ec0a82cdccf040b6e6a006aea6cb63128f3
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-28 17:27:10 +0200

    Create NumberOfOnes.java

---
+++ one-line-java/NumberOfOnes.java
@@ -0,0 +1,9 @@
+public class NumberOfOnes {
+
+    public static void main(String[] args) {
+        
+        System.out.println(
+            Integer.bitCount(
+                new java.util.Scanner(System.in).nextInt()));
+    }
+}
