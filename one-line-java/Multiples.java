d23d335d5e452012b09018b04bdf159da7958be6
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-28 17:23:38 +0200

    Create Multiples.java

---
+++ one-line-java/Multiples.java
@@ -0,0 +1,10 @@
+public class Multiples { 
+
+    public static void main(String[] args) {
+        
+        System.out.print(java.util.stream.IntStream
+            .range(3, new java.util.Scanner(System.in).nextInt())
+            .filter(i -> i % 3 == 0 || i % 5 == 0)
+            .sum());
+    }
+}
