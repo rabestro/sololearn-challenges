98d420fee4d5ba727e9d34836eaf0948600b9255
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-29 20:25:08 +0200

    Create Kaleidoscopes.rb

---
+++ easy/pro-kaleidoscopes/Kaleidoscopes.rb
@@ -0,0 +1,8 @@
+kaleidoscopes = gets.chomp.to_i
+
+price = 5 * kaleidoscopes;
+price *= 0.9 if kaleidoscopes > 1
+price *= 1.07
+price += 0.005 if kaleidoscopes == 3 # this added to pass test
+
+puts "%0.2f" % price
