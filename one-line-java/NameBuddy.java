272353822364d2fad7302b5bec8ec5d612836907
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-30 01:06:12 +0200

    Create NameBuddy.java

---
+++ one-line-java/NameBuddy.java
@@ -0,0 +1,14 @@
+public class NameBuddy {
+    public static void main(String[] args) {
+           
+        System.out.print(java.util.regex.Pattern.matches(".*(.).*\\1$",
+            new java.util.Scanner(System.in)
+                .useDelimiter("\\s")
+                .tokens()
+                .map(name -> name.substring(0,1))
+                .reduce((a,b) -> a = a + b)
+                .orElse("")) ? "Compare notes" : "No such luck");
+    }
+}
+
+
