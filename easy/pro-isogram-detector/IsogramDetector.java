2026a69e1efe100f0d56ac359473f875a42d6e72
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-29 12:36:06 +0200

    Create IsogramDetector.java

---
+++ easy/pro-isogram-detector/IsogramDetector.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class IsogramDetector {
+	public static void main(String[] args) {
+	
+	    final var input = new Scanner(System.in);
+        final var word = input.next();
+        input.close();
+        
+        final var isIsogram = !Pattern.matches(".*(.).*\\1.*", word);
+        
+	    System.out.println(isIsogram);
+	}
+}
