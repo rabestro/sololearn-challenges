b4b1eb9728e73f1f298e76b926e15d519fe1e76a
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-29 20:14:10 +0200

    Create kaleidoscopes.c

---
+++ easy/pro-kaleidoscopes/kaleidoscopes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int kaleidoscopes;
+    scanf("%d", &kaleidoscopes);
+    
+    double price = 5 * kaleidoscopes;
+    
+    if (kaleidoscopes > 1) {
+    	price *= 0.9;
+    }
+    price *= 1.07;
+    
+    printf("%.2f", price);
+    
+    return 0;
+}
