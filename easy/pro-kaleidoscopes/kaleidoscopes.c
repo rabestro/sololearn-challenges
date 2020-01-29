633a8b613441b0ce70c1aad23b2a5da02b7b8352
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-29 20:27:13 +0200

    Update kaleidoscopes.c

--- easy/pro-kaleidoscopes/kaleidoscopes.c
+++ easy/pro-kaleidoscopes/kaleidoscopes.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
 int main() {
-
     int kaleidoscopes;
     scanf("%d", &kaleidoscopes);
     
     double price = 5 * kaleidoscopes;
-    
-    if (kaleidoscopes > 1) {
-    	price *= 0.9;
-    }
+    if (kaleidoscopes > 1) price *= 0.9;
     price *= 1.07;
-    
     printf("%.2f", price);
     
     return 0;
