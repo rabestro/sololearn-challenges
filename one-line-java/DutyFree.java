d4cf75ab794951556cbc0c8ee2bc6f848e2418f7
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-30 00:33:48 +0200

    Create DutyFree.java
    Issue #2

---
+++ one-line-java/DutyFree.java
@@ -0,0 +1,13 @@
+public class DutyFree {
+    public static void main(String[] args) {
+           
+        System.out.print(
+            new java.util.Scanner(System.in)
+                .useDelimiter(" ")
+                .tokens()
+                .mapToDouble(Double::parseDouble)
+                .map(x -> 1.1 * x)
+                .anyMatch(x -> x > 20)
+                ? "Back to the store" : "On to the terminal");
+    }
+}
