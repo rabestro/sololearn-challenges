8c7794c1921d5eec976c16638ec37a3e8e5ce5db
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-29 14:13:17 +0200

    Create Kaleidoscopes.java

---
+++ easy/pro-kaleidoscopes/Kaleidoscopes.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Kaleidoscopes {
+    public static void main(final String[] args) {
+        
+        final var input = new Scanner(System.in);
+        final var kaleidoscopes = input.nextInt();
+        input.close();
+        
+        final var discount = 0.9;
+        final var tax = 1.07;
+        
+        double price = kaleidoscopes * 5;
+        
+        if (kaleidoscopes > 1) {
+        	price *= discount;
+        }
+        
+        price *= tax;
+        
+        System.out.printf("%.2f", 
+            new BigDecimal(price).setScale(2, RoundingMode.HALF_EVEN));
+    }
+}
