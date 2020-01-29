d26b8def5890ca7b6efe67a8be238608f8fcd7f7
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-29 13:41:12 +0200

    Update SkeeBall.java

--- easy/skee-ball/SkeeBall.java
+++ easy/skee-ball/SkeeBall.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
-public class SkeeBall
-{
+public class SkeeBall {
     public static void main(String[] args) {
+        
         final var input = new Scanner(System.in);
         final var points = input.nextInt();
         final var price = input.nextInt();
