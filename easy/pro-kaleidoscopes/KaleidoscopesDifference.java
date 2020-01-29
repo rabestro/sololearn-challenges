ff1fb416d9ad45d6cf0e6d64961ba2c8428cb7ed
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-29 15:26:41 +0200

    Create KaleidoscopesDifference.java

---
+++ easy/pro-kaleidoscopes/KaleidoscopesDifference.java
@@ -0,0 +1,40 @@
+package com.rabestro.sololearn;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.stream.IntStream;
+
+public class KaleidoscopesDifference {
+
+    public static void main(String[] args) {
+        IntStream.range(2, 1000)
+                .filter(i -> !big(i).equals(small(i))).forEach(i -> System.out.printf("%6d %10s %10s %n", i, big(i),small(i) ));
+    }
+
+    static BigDecimal small(int kaleidoscopes) {
+        double discount = 0.9;
+        double tax = 1.07;
+
+        double price = kaleidoscopes * 5;
+
+        if (kaleidoscopes > 1) {
+            price *= discount;
+        }
+        price *= tax;
+        return new BigDecimal(price).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    static BigDecimal big(int kaleidoscopes) {
+        final var discount = new BigDecimal(0.9);
+        final var tax = new BigDecimal(1.07);
+
+        var price = new BigDecimal(kaleidoscopes * 5);
+
+        if (kaleidoscopes > 1) {
+            price = price.multiply(discount);
+        }
+
+        price = price.multiply(tax);
+        return price.setScale(2, RoundingMode.HALF_EVEN);
+    }
+}
