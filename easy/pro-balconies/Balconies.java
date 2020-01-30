def104e1855532575dd41328cd172085cac10600
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-30 17:54:44 +0200

    Update Balconies.java

--- easy/pro-balconies/Balconies.java
+++ easy/pro-balconies/Balconies.java
@@ -8,8 +8,9 @@ Task
 Evaluate the area of two different balconies and determine which one is bigger.
 
 Input Format 
-Your inputs are two strings where the measurements for height and width are separated by a comma. 
-The first one represents apartment A, the second represents apartment B.
+Your inputs are two strings where the measurements for height and width 
+are separated by a comma. The first one represents apartment A, 
+the second represents apartment B.
 
 Output Format: 
 A string that says whether apartment A or apartment B has a larger balcony.
