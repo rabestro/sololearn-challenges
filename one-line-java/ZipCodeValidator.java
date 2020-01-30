25ce48106e51bd30e255a59c2cc203d4bdb606e4
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-30 15:46:42 +0200

    Update ZipCodeValidator.java

--- one-line-java/ZipCodeValidator.java
+++ one-line-java/ZipCodeValidator.java
@@ -1,8 +1,7 @@
 public class ZipCodeValidator {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 	    
-	    System.out.println(java.util.regex.Pattern
-	        .matches("\\d{5}", new java.util.Scanner(System.in).nextLine()));
-	}
+        System.out.println(java.util.regex.Pattern
+	    .matches("\\d{5}", new java.util.Scanner(System.in).nextLine()));
+    }
 }
