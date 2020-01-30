f1aa6cb10494c332e019daf9ac513338639b0a2e
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-01-30 14:01:42 +0200

    Update Hovercraft.java

--- easy/hovercraft/Hovercraft.java
+++ easy/hovercraft/Hovercraft.java
@@ -1,10 +1,14 @@
 /*
 Hovercraft
 
-You run a hovercraft factory. Your factory makes ten hovercrafts in a month. Given the number of customers you got that month, did you make a profit? It costs you 2,000,000 to build a hovercraft, and you are selling them for 3,000,000. You also pay 1,000,000 each month for insurance.
+You run a hovercraft factory. Your factory makes ten hovercrafts in a month. 
+Given the number of customers you got that month, did you make a profit? 
+It costs you 2,000,000 to build a hovercraft, and you are selling them for 3,000,000. 
+You also pay 1,000,000 each month for insurance.
 
 Task: 
-Determine whether or not you made a profit based on how many of the ten hovercrafts you were able to sell that month.
+Determine whether or not you made a profit based on how many of the ten hovercrafts 
+you were able to sell that month.
  
 Input Format: 
 An integer that represents the sales that you made that month.
