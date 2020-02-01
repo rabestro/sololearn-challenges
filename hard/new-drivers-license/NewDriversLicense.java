f8fe4925452ea333ba48b649037ec3758f64f1da
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-01 22:07:55 +0200

    Update NewDriversLicense.java

--- hard/new-drivers-license/NewDriversLicense.java
+++ hard/new-drivers-license/NewDriversLicense.java
@@ -1,11 +1,11 @@
 public class NewDriversLicense {
     public static void main(String[] args) {
-        var input = new java.util.Scanner(System.in);
-        var hero = input.next();
-        var agents = input.nextInt();
+        final var input = new java.util.Scanner(System.in);
+        final var hero = input.next();
+        final var agents = input.nextInt();
         
-        var position = input.tokens().filter(person -> person.compareTo(hero) < 0).count();
-        var time = 20 * (1 + position / agents);
+        final var position = input.tokens().filter(person -> person.compareTo(hero) < 0).count();
+        final var time = 20 * (1 + position / agents);
         
         System.out.print(time);
     }
