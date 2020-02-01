e223191179f0227a894ab45c342fa25f2bdcfcd6
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-01 16:33:09 +0200

    Update SplittingStrings.java

--- medium/pro-splitting-strings/SplittingStrings.java
+++ medium/pro-splitting-strings/SplittingStrings.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class SplittingStrings
-{
+public class SplittingStrings {
     public static void main(String[] args) {
         final var in = new Scanner(System.in);
         final var s = in.nextLine();
