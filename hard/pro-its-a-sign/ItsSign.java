55ec1f9dafc1d47f93e6d2a978a4c4ba2b5c3f1f
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-01 18:25:11 +0200

    Create ItsSign.java

---
+++ hard/pro-its-a-sign/ItsSign.java
@@ -0,0 +1,11 @@
+public class ItsSign {
+    public static void main(String[] args) {
+	    
+        System.out.println(new java.util.Scanner(System.in)
+            .tokens()
+            .limit(4)
+            .map(StringBuilder::new)
+            .anyMatch(w -> w.toString().equals(w.reverse().toString())) 
+                ? "Open" : "Trash");
+    }
+}
