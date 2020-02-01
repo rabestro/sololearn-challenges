2216d4997efe1abfbf3a2343eb6f20820e95e0a2
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-01 18:17:42 +0200

    Update PasswordValidation.java

--- hard/password-validation/PasswordValidation.java
+++ hard/password-validation/PasswordValidation.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class PasswordValidation
-{
+public class PasswordValidation {
     public static void main(String[] args) {
         final var input = new Scanner(System.in);
         final var password = input.nextLine();
