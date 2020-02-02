770f16aa2233c82a7845b9313993d999f7b22436
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-02 13:37:54 +0200

    Create flamingos.c

---
+++ easy/guard-flamingos/flamingos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int length, width;
+    scanf("%d\n%d", &length, &width);
+    int flamingos = length + width;
+    printf("%d", flamingos);
+    return 0;
+}
