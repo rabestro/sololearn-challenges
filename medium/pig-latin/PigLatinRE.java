90ef7bd55c0032d08997731836861d61a234b3e0
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-06 00:51:46 +0200

    Create PigLatinRE.java

---
+++ medium/pig-latin/PigLatinRE.java
@@ -0,0 +1,9 @@
+public class PigLatinRE {
+	public static void main(String[] args) {
+
+		System.out.println(
+		    new java.util.Scanner(System.in)
+		        .nextLine()
+		        .replaceAll("(\\S)(\\S*)", "$2$1ay"));
+	}
+}
