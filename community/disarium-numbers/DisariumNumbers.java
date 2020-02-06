e3c456fedceadcb80f381b6e1a93ee5dc2dfdb8c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-02-06 16:33:33 +0200

    Refactoring

--- community/disarium-numbers/DisariumNumbers.java
+++ community/disarium-numbers/DisariumNumbers.java
@@ -16,28 +16,28 @@ import java.util.stream.IntStream;
 
 public class DisariumNumbers {
 
-	public static void main(String[] args) {
-		final var sc = new Scanner(System.in);
-		final var lowerBound = sc.nextInt();
-		final var isRange = sc.hasNextInt();
-		final var upperBound = isRange ? sc.nextInt() : lowerBound;
-		sc.close();
-		
-		if (isRange) {
-			IntStream.range(lowerBound, upperBound)
-				.filter(DisariumNumbers::isDisarium)
-				.forEach(System.out::println);
-		} else {
-			System.out.println(isDisarium(lowerBound));
-		}
-	}
-	
-	public static boolean isDisarium(int n) {
-		final var digits = Integer.toString(n).chars().map(c -> c - '0').toArray();
-		for (int p, digit, power = digits.length; power > 0 && n >=0 ; n -= p) {
-			p = digit = digits[--power];
-			for (int i = power; i-- > 0; p *= digit);
-		}
-		return n == 0;
-	}
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+        final var lowerBound = sc.nextInt();
+        final var isRange = sc.hasNextInt();
+        final var upperBound = isRange ? sc.nextInt() : lowerBound;
+        sc.close();
+
+        if (isRange) {
+            IntStream.range(lowerBound, upperBound)
+                    .filter(DisariumNumbers::isDisarium)
+                    .forEach(System.out::println);
+        } else {
+            System.out.println(isDisarium(lowerBound));
+        }
+    }
+
+    public static boolean isDisarium(int n) {
+        final var digits = Integer.toString(n).chars().map(c -> c - '0').toArray();
+        for (int p, digit, power = digits.length; power > 0 && n >= 0; n -= p) {
+            p = digit = digits[--power];
+            for (int i = power; i-- > 0; p *= digit) ;
+        }
+        return n == 0;
+    }
 }
