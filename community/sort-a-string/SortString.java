c0a99609118631781839a60af1c782a717adba13
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-06 19:50:42 +0200

    Create SortString.java

---
+++ community/sort-a-string/SortString.java
@@ -0,0 +1,19 @@
+/*
+  Given a string as input, output its characters alphabetically sorted from A to Z.
+  Whitespaces should be ignored.
+*/
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SortString {
+	public static void main(String[] args) {
+		final var in = new Scanner(System.in);
+		final var string = in.nextLine();
+		
+		final var temp = string.replaceAll("\\s", "").toCharArray();
+		Arrays.sort(temp);
+		
+		final var sortedString = new String(temp);
+		System.out.println(sortedString);
+	}
+}
