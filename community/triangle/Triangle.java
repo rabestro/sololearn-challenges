32814aee39036fc6b2395da4ed36581cdb639322
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-06 15:43:07 +0200

    Create Triangle.java

---
+++ community/triangle/Triangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        
+        final var input = new Scanner(System.in);
+        final var a = input.nextInt();
+        final var b = input.nextInt();
+        final var c = input.nextInt();
+        input.close();
+        
+        final var isValidTriangle = a + b > c && a + c > b && b + c > a;
+        
+        System.out.print(isValidTriangle);
+        
+    }
+}
