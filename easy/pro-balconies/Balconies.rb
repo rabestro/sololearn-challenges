4329405b617373230ef992fb1ad7b64b20de1ab3
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-12 22:03:45 +0200

    Update Balconies.rb

--- easy/pro-balconies/Balconies.rb
+++ easy/pro-balconies/Balconies.rb
@@ -1,8 +1,6 @@
 aWidth, aLength = gets.chomp.split(",")
 bWidth, bLength = gets.chomp.split(",")
 
-if (aWidth.to_i * aLength.to_i) > (bWidth.to_i * bLength.to_i)
-    print("Apartment A")
-else
-    print("Apartment B")
-end
+isBiggerA = aWidth.to_i * aLength.to_i > bWidth.to_i * bLength.to_i
+
+puts "Apartment " + (isBiggerA ? "A": "B")
