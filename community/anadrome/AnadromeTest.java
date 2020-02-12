52175f097332bd508893ad16051596699ab4d031
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-02-12 19:50:45 +0200

    Updated the test for Anadrome

--- community/anadrome/AnadromeTest.java
+++ community/anadrome/AnadromeTest.java
@@ -1,66 +1,82 @@
-import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import java.io.InputStream;
-import java.io.PrintStream;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.Arrays;
+import java.util.Collection;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
 /**
  * The test class AnadromeTest.
  *
- * @author  Jegors Čemisovs 
+ * @author Jegors Čemisovs
  * @version 2020-02-10
  */
-public class AnadromeTest
-{
-    private static PrintStream originalOut;
-    private static InputStream originalIn;
 
-    private ByteArrayOutputStream out;
+@RunWith(Parameterized.class)
+public class AnadromeTest {
 
-    /**
-     * Default constructor for test class AnadromeTest
-     */
-    public AnadromeTest()
-    {
+    private final InputStream originalIn = System.in;
+    private final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    private final String input;
+    private final String expected;
+
+    public AnadromeTest(String input, String expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"SoloSolo", "yes"}, 
+                {"3haha", "yes"}, 
+                {"Solo", "no"},
+                {"BugaMuga", "yes"},
+                {"catycat", "yes"}
+        });
     }
 
     /**
      * Sets up the test fixture.
-     *
+     * <p>
      * Called before every test case method.
      */
     @Before
-    public void setUp()
-    {
-        originalOut = System.out;
-        originalIn = System.in;
+    public void setUp() {
+        System.setOut(new PrintStream(out));
     }
-    
+
     @Test
     public void testExampeOne() {
-        final String expected = "yes";
-        var in = new ByteArrayInputStream("SoloSolo".getBytes());
-        this.out = new ByteArrayOutputStream(expected.length() * 2);
-
+        System.err.printf("In:  %s%nOut: %s%n", input, expected);
+        final var in = new ByteArrayInputStream(this.input.getBytes());
         System.setIn(in);
-        System.setOut(new PrintStream(this.out));
-        
         Anadrome.main(null);
-        assertEquals(expected, this.out.toString());
+        assertEquals(expected, out.toString());
+        System.err.printf("Results: %s / %s%n", expected, out.toString());
     }
 
     /**
      * Tears down the test fixture.
-     *
+     * <p>
      * Called after every test case method.
      */
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
         System.setOut(originalOut);
-        System.setIn(originalIn);    
+        System.setIn(originalIn);
     }
 }
 
