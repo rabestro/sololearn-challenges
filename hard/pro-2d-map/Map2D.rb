4016ff067dbd06c6deb5805602f099e61ad660d6
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-13 21:18:22 +0200

    Create Map2D.rb

---
+++ hard/pro-2d-map/Map2D.rb
@@ -0,0 +1,9 @@
+SIZE = 5 + 1 # Size of square + comma
+
+map = gets
+indexA = map.index('P')
+indexB = map.index('P', indexA + 1)
+       
+path = indexB / SIZE - indexA / SIZE + (indexA % SIZE - indexB % SIZE).abs
+        
+puts path
