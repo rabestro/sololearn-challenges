d2cb46a4c368aca8a6e19c241f3788b9f3bd053a
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-13 14:23:22 +0200

    Update BallparkOrders.rb

--- easy/pro-ballpark-orders/BallparkOrders.rb
+++ easy/pro-ballpark-orders/BallparkOrders.rb
@@ -1,6 +1,7 @@
-menu = {Nachos: 6, Pizza: 6, Cheeseburger: 10, Water: 4, Coke: 5}
 tax = 1.07
+menu = {Nachos: 6, Pizza: 6, Cheeseburger: 10, Water: 4, Coke: 5}
+menu.default = menu[:Coke]
 
-total_cost = gets.split.reduce(0) {|sum, item| sum += menu.fetch(item.to_sym, 5)}
+total_cost = gets.split.map {|i| menu[i.to_sym]}.inject(:+)
 
-print '%.2f' % [total_cost * tax]
+puts '%.2f' % [total_cost * tax]
