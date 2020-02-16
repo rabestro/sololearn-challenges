95f25f15eae73146bc2615ba9917cd0a3dc4dd3a
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-16 21:02:47 +0200

    Update snap_crackle_pop.c

--- medium/pro-snap-crackle-and-pop/snap_crackle_pop.c
+++ medium/pro-snap-crackle-and-pop/snap_crackle_pop.c
@@ -4,7 +4,7 @@ int main() {
     int i;
     
     while(scanf("%d\n", &i) == 1)
-        printf(i % 3 == 0 ? "Pop " : i % 2 == 0 ? "Crackle " : "Snap ");
+        printf(i % 3 ? i % 2 ? "Snap " : "Crackle " : "Pop ");
         
     return 0;
 }
