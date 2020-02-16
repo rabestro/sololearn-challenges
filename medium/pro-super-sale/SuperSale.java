50183a1151bea566422c7b9c63ded7c9f8b8cfcf
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-16 20:50:53 +0200

    Update SuperSale.java

--- medium/pro-super-sale/SuperSale.java
+++ medium/pro-super-sale/SuperSale.java
@@ -1,14 +1,16 @@
+import java.util.Scanner;
+
 public class SuperSale {
     public static void main(String[] args) {
-
-        System.out.printf("%d", (int) (0.3 * 1.07 *
-                -new java.util.Scanner(System.in)
-                        .useDelimiter(",")
-                        .tokens()
-                        .mapToDouble(Double::parseDouble)
-                        .map(x -> -x)
-                        .sorted()
-                        .skip(1)
-                        .sum()));
+        final var in = new Scanner(System.in).useDelimiter(",");
+        float item, max = 0, sum = 0;
+        
+        while (in.hasNextFloat()) {
+        	item = in.nextFloat();
+        	if (item > max) max = item;
+        	sum += item;
+        }
+        
+        System.out.printf("%d", (int) (0.3 * 1.07 * (sum - max)));
     }
 }
