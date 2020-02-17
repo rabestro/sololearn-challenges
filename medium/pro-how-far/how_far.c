02943dc47bc5e1974eef547408968e589de93e77
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-17 02:08:46 +0200

    Create how_far.c

---
+++ medium/pro-how-far/how_far.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n = 0;
+    for (char c; scanf("%c", &c) == 1; ) 
+        switch (c) {
+            case 'B': if (n) ++n; break;
+        	case 'H': 
+        	case 'P': if (n++) {printf("%d", n - 2); return 0;}
+        }
+    return 0;
+}
