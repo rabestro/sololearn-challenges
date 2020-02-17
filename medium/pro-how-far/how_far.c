02a649cf19c8ac71422324b1eb3b2e65b7924b79
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-17 02:09:22 +0200

    Update how_far.c

--- medium/pro-how-far/how_far.c
+++ medium/pro-how-far/how_far.c
@@ -5,8 +5,8 @@ int main() {
     for (char c; scanf("%c", &c) == 1; ) 
         switch (c) {
             case 'B': if (n) ++n; break;
-        	case 'H': 
-        	case 'P': if (n++) {printf("%d", n - 2); return 0;}
+            case 'H': 
+            case 'P': if (n++) {printf("%d", n - 2); return 0;}
         }
     return 0;
 }
