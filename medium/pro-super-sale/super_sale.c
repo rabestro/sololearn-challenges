065e41c24d1666254d88123facf4042130454b10
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-02-17 16:43:32 +0200

    Update super_sale.c

--- medium/pro-super-sale/super_sale.c
+++ medium/pro-super-sale/super_sale.c
@@ -2,10 +2,8 @@
 
 int main() {
     float item, sum, max;
-    
     for (max = sum = 0; 1 == scanf("%f,", &item); sum += item) 
         if (item > max) max = item;
-        
     int save = (sum - max) * 0.3 * 1.07;
     
     printf("%d", save);
