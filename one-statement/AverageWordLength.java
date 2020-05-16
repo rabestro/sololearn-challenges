24facdf21d81c2b2799cb64c8e5def26bdd27f28
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-05-16 13:28:42 +0300

    Update AverageWordLength.java

--- one-statement/AverageWordLength.java
+++ one-statement/AverageWordLength.java
@@ -1,11 +1,11 @@
+import static java.util.stream.Collectors.averagingInt;
+
 public class AverageWordLength {
-    
     public static void main(String[] args) {
-        
         System.out.print((int)Math.ceil( 
             new java.util.Scanner(System.in)
                 .useDelimiter("\\W+")
                 .tokens()
-                .collect(java.util.stream.Collectors.averagingInt(String::length))));
+                .collect(averagingInt(String::length))));
     }
 }
