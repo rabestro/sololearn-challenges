e1847ffc7fb118f15fde2f77d6fbc3fe52d26a37
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-05-16 13:34:09 +0300

    Update DejaVu.java

--- one-statement/DejaVu.java
+++ one-statement/DejaVu.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
-import java.util.regex.Pattern;
-
-public class DejaVu
-{
-    public static void main(String[] args) {
-	    
+public class DejaVu {
+    public static void main(String[] args) {  
        System.out.println(
-           Pattern.matches(".*(.).*\\1.*", new Scanner(System.in).nextLine()) 
-           ? "Deja Vu" : "Unique");
+           new java.util.Scanner(System.in)
+	       .nextLine()
+	       .matches(".*(.).*\\1.*") ? "Deja Vu" : "Unique"
+       );
     }
 }
