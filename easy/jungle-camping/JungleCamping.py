13c1073eb9c48dff991b1db1103c0019a54a6874
coder135792468 <56873740+coder135792468@users.noreply.github.com>
2020-05-26 07:59:57 +0530

    Update JungleCamping.py

--- easy/jungle-camping/JungleCamping.py
+++ easy/jungle-camping/JungleCamping.py
@@ -1,4 +1,10 @@
+"""
 animals = {"Grr": "Lion", "Rawr": "Tiger", "Ssss": "Snake", "Chirp": "Bird"}
 
 noise = input().split()
 print(' '.join(map(animals.get, noise)))
+
+"""
+
+
+print(input().replace("Grr","Lion").replace("Rawr","Tiger").replace("Chirp","Bird").replace("Ssss","Snake"))
