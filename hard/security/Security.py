be8e532688559ba978b3e3280f1e63bc79c1eefb
PsychoRover <69534855+PsychoRover@users.noreply.github.com>
2020-08-14 12:17:17 +0300

    Update Security.py

--- hard/security/Security.py
+++ hard/security/Security.py
@@ -1,4 +1,7 @@
 import re
-floor = input()
-alarm = re.search("[$T].*G.*[$T]", floor) is None
-print(('quiet','ALARM')[alarm])
+floor = "".join(re.findall(r'[G$T]', 'xxxxxGxTx$xGxxT'))
+if re.search(r'(\$T)|(T\$)', floor):
+    print('ALARM')
+else:
+    print('quiet')
+
