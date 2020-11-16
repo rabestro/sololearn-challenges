d01dba0b97e65abf450ad4647327fc031c5c62d3
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-16 10:39:56 +0200

    Update EasterEggs.java

--- easy/pro-easter-eggs/EasterEggs.java
+++ easy/pro-easter-eggs/EasterEggs.java
@@ -1,13 +1,15 @@
-public class EasterEggs {
+public final class EasterEggs {
     public static void main(final String[] args) {
-        final var input = new java.util.Scanner(System.in);
-        final var totalNumberEggs = input.nextInt();
-        final var yourAmount = input.nextInt();
-        final var friendsAmount = input.nextInt();
-        input.close();
+        final var scanner = new java.util.Scanner(System.in);
+        
+        final var totalNumberEggs = scanner.nextInt();
+        final var yourAmount = scanner.nextInt();
+        final var friendsAmount = scanner.nextInt();
         
         final var isStillEggs = totalNumberEggs > yourAmount + friendsAmount;
         
-        System.out.print(isStillEggs ? "Keep Hunting" : "Candy Time");
+        System.out.println(
+            isStillEggs ? "Keep Hunting" : "Candy Time"
+        );
     }
 }
