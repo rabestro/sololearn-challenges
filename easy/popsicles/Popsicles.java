9d7cafe6c022333f6f26e416d24f5a61294af2d4
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-16 10:07:08 +0200

    Update Popsicles.java

--- easy/popsicles/Popsicles.java
+++ easy/popsicles/Popsicles.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
-public class Popsicles {
+public final class Popsicles {
     public static void main(String[] args) {
-        
-        final var input = new Scanner(System.in);
-        final var siblings = input.nextInt();
-        final var popsicles = input.nextInt();
-        input.close();
+        final var scanner = new Scanner(System.in);
+        final var siblings = scanner.nextInt();
+        final var popsicles = scanner.nextInt();
         
         final var isEvenAmount = popsicles % siblings == 0;
         
-        if (isEvenAmount) {
-            System.out.print("give away");
-        } else {
-            System.out.print("eat them yourself");
-        }
+        System.out.println(
+            isEvenAmount ? "give away" : "eat them yourself"
+        );
     }
 }
