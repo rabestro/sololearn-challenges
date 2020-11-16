30df5d9a4fa9cb4dd274cf80e42b2f1d5d570fe8
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-16 10:37:30 +0200

    Update Candles.java

--- easy/pro-candles/Candles.java
+++ easy/pro-candles/Candles.java
@@ -1,9 +1,6 @@
-public class Candles {
+public final class Candles {
     public static void main(String[] args) {
-        final var in = new java.util.Scanner(System.in);
-        final int friends = in.nextInt();
-        in.close();
-        
+        final int friends = new java.util.Scanner(System.in).nextInt();
         final int candles = 9 * (1 + friends);
         
         System.out.print(candles);
