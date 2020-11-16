5a0803a0b80c327f70cce6808149017510f5345e
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-16 09:59:30 +0200

    Update SafetyDepositBoxes.java
    Refactoring

--- medium/pro-safety-deposit-boxes/SafetyDepositBoxes.java
+++ medium/pro-safety-deposit-boxes/SafetyDepositBoxes.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class SafetyDepositBoxes {
     public static void main(String[] args) {
-        final var input = new Scanner(System.in);
-        final var items = input.nextLine();
-        final var lookingFor = input.nextLine();
-        input.close();
+        final var scanner = new Scanner(System.in);
+        final var items = scanner.nextLine();
+        final var lookingFor = scanner.nextLine();
 
-        final int itemsBeforeDesired = items.substring(0, items.indexOf(lookingFor))
-                .replaceAll("[^,]", "").length();
+        final int itemsBeforeDesired = items
+            .substring(0, items.indexOf(lookingFor))
+            .replaceAll("[^,]", "").length();
 
         final int time = 5 + 5 * itemsBeforeDesired;
 
