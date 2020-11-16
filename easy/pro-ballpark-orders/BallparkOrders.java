abfe5fd76d413c2d5dd3d18020b60c9d4ede8778
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-16 10:23:52 +0200

    Update BallparkOrders.java

--- easy/pro-ballpark-orders/BallparkOrders.java
+++ easy/pro-ballpark-orders/BallparkOrders.java
@@ -4,14 +4,14 @@ import java.math.BigDecimal;
 
 public final class BallparkOrders {
     public static void main(String[] args) {
-        
-        final Map<String, Integer> menu = Map.of(
+        final var menu = Map.of(
             "Nachos", 6, "Pizza", 6, "Cheeseburger", 10, "Water", 4, "Coke", 5);
-            
+        
         final var tax = new BigDecimal("1.07");
+        
         final var sum = new Scanner(System.in)
             .tokens()
-            .mapToInt(item -> menu.get(menu.containsKey(item) ? item : "Coke"))
+            .mapToInt(item -> menu.getOrDefault(item, menu.get("Coke")))
             .sum();
             
         final var totalCost = new BigDecimal(sum).multiply(tax);
