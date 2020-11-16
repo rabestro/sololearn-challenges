42191fe8d73fdde46b84a53588dd78a501814a12
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-16 10:34:06 +0200

    Update Argentina.java

--- easy/argentina/Argentina.java
+++ easy/argentina/Argentina.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class Argentina {
+public final class Argentina {
     public static void main(final String[] args) {
+        final var scanner = new Scanner(System.in);
+        final var pesos = scanner.nextInt();
+        final var dollars = scanner.nextInt();
         
-        final var input = new Scanner(System.in);
-        final var pesos = input.nextInt();
-        final var dollars = input.nextInt();
-        input.close();
-        
-        System.out.print(dollars * 50l > pesos ? "Pesos" : "Dollars");
+        System.out.println(
+            dollars * 50l > pesos ? "Pesos" : "Dollars"
+        );
     }
 }
