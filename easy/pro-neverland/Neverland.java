0c73ad08f72a04a4ab14247faba6ba3060d67ec6
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-16 10:04:07 +0200

    Update Neverland.java

--- easy/pro-neverland/Neverland.java
+++ easy/pro-neverland/Neverland.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class Neverland {
+public final class Neverland {
     public static void main(String[] args) {
+        final var scanner = new Scanner(System.in);
+        final var yourAge = scanner.nextInt();
+        final var years = scanner.nextInt();
         
-        final var input = new Scanner(System.in);
-        final var yourAge = input.nextInt();
-        final var years = input.nextInt();
-        input.close();
-        
-        System.out.printf("My twin is %d years old and they are %d years older than me", yourAge + years, years);
+        System.out.printf(
+            "My twin is %d years old and they are %d years older than me", 
+            yourAge + years, years);
     }
 }
