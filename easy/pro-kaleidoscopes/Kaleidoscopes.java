40dafdb6e8d3f7cc62d1342469e62b7504b87e64
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-18 22:21:22 +0200

    Update Kaleidoscopes.java

--- easy/pro-kaleidoscopes/Kaleidoscopes.java
+++ easy/pro-kaleidoscopes/Kaleidoscopes.java
@@ -4,20 +4,17 @@ import java.math.RoundingMode;
 
 public class Kaleidoscopes {
     public static void main(final String[] args) {
-        
-        final var input = new Scanner(System.in);
-        final var kaleidoscopes = input.nextInt();
-        input.close();
+        final var scanner = new Scanner(System.in);
+        final var kaleidoscopes = scanner.nextInt();
         
         final var discount = 0.9;
         final var tax = 1.07;
         
-        double price = kaleidoscopes * 5;
+        var price = kaleidoscopes * 5.0;
         
         if (kaleidoscopes > 1) {
         	price *= discount;
         }
-        
         price *= tax;
         
         System.out.printf("%.2f", 
