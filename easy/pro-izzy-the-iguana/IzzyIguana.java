7f6e94bcc081a55e1db1d05949ade7e347829127
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-18 22:18:55 +0200

    Update IzzyIguana.java

--- easy/pro-izzy-the-iguana/IzzyIguana.java
+++ easy/pro-izzy-the-iguana/IzzyIguana.java
@@ -3,14 +3,8 @@ import java.util.Scanner;
 
 public class IzzyIguana {
     public static void main(String[] args) {
-	    
-        final Map<String, Integer> snackPoints = Map.of(
-            "Lettuce", 5, "Carrot", 4, "Mango", 9, "Cheeseburger", 0);
-            
-        final int sum = new Scanner(System.in)
-	    .tokens()
-            .mapToInt(snackPoints::get)
-            .sum();
+        final var snackPoints = Map.of("Lettuce", 5, "Carrot", 4, "Mango", 9, "Cheeseburger", 0);
+        final var sum = new Scanner(System.in).tokens().mapToInt(snackPoints::get).sum();
             
         System.out.println(sum >= 10 ? "Come on Down!" : "Time to wait");
     }
