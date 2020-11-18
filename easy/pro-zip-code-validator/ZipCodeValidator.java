5c9dad61390c051068ffc78e65626894101a891c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-18 22:27:11 +0200

    Update ZipCodeValidator.java

--- easy/pro-zip-code-validator/ZipCodeValidator.java
+++ easy/pro-zip-code-validator/ZipCodeValidator.java
@@ -2,13 +2,10 @@ import java.util.regex.Pattern;
 
 public class ZipCodeValidator {
     public static void main(String[] args) {
-	    
-        final var input = new java.util.Scanner(System.in);
-        final var zipCode = input.nextLine();
-        input.close();
+        final var scanner = new java.util.Scanner(System.in);
+        final var zipCode = scanner.nextLine();
 	    
         final var isValid = Pattern.matches("\\d{5}", zipCode);
-	    
         System.out.println(isValid);
     }
 }
