cff0091f599be5a81781ee73caee8ea9b413007c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-19 10:54:29 +0200

    Update Roadrunner.java

--- medium/pro-roadrunner/Roadrunner.java
+++ medium/pro-roadrunner/Roadrunner.java
@@ -1,23 +1,14 @@
-import java.util.Scanner;
-
-public class Roadrunner {
+public final class Roadrunner {
     public static void main(String[] args) {
-
-        final var input = new Scanner(System.in);
+        final var scanner = new java.util.Scanner(System.in);
         final var distanceApart = 50;
-        final var distanceSafety = input.nextInt();
-        final var speedRoadrunner = input.nextInt();
-        final var speedCoyote = input.nextInt();
-        input.close();
+        final var distanceSafety = scanner.nextInt();
+        final var speedRoadrunner = scanner.nextInt();
+        final var speedCoyote = scanner.nextInt();
 
         final var timeToSafety = (double) distanceSafety / speedRoadrunner;
-
         final var isCoyoteCaughtRoadrunner = speedCoyote > (distanceApart + distanceSafety) / timeToSafety;
-
-        if (isCoyoteCaughtRoadrunner) {
-            System.out.print("Yum");
-        } else {
-            System.out.print("Meep Meep");
-        }
+        
+        System.out.print(isCoyoteCaughtRoadrunner ? "Yum" : "Meep Meep");
     }
 }
