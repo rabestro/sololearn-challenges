ceb65250801ecd730c89e2fb4cf4039adb43e4de
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-19 10:47:54 +0200

    Update ZipCodeValidator.java

--- easy/pro-zip-code-validator/ZipCodeValidator.java
+++ easy/pro-zip-code-validator/ZipCodeValidator.java
@@ -1,11 +1,8 @@
-import java.util.regex.Pattern;
-
-public class ZipCodeValidator {
+public final class ZipCodeValidator {
     public static void main(String[] args) {
         final var scanner = new java.util.Scanner(System.in);
         final var zipCode = scanner.nextLine();
-	    
-        final var isValid = Pattern.matches("\\d{5}", zipCode);
+        final var isValid = zipCode.matches("\\d{5}");
         System.out.println(isValid);
     }
 }
