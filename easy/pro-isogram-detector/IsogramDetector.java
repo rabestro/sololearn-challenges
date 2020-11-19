7fff2a8fe8f6ef3c64bea2c28e1c61d4b000d04f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-19 10:43:07 +0200

    Update IsogramDetector.java

--- easy/pro-isogram-detector/IsogramDetector.java
+++ easy/pro-isogram-detector/IsogramDetector.java
@@ -1,13 +1,7 @@
-import java.util.Scanner;
-import java.util.regex.Pattern;
-
 public final class IsogramDetector {
     public static void main(String[] args) {
-        final var input = new Scanner(System.in);
-	final var word = input.next();
-        
-        final var isIsogram = !Pattern.matches(".*(.).*\\1.*", word);
-        
+        final var word = new java.util.Scanner(System.in).next();
+        final var isIsogram = !word.matches(".*(.).*\\1.*");
         System.out.println(isIsogram);
     }
 }
