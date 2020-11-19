c83685e45e65ea98c0b0ff7bd87d66eaf71662b1
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-19 14:17:51 +0200

    Create cheer_creator.cpp

---
+++ easy/cheer-creator/cheer_creator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int yards;
+    cin >> yards;
+    
+    if (yards > 10) cout << "High Five";
+    else if (yards < 1) cout << "shh";
+    else while (yards--) cout << "Ra!";
+    
+	return 0;
+}
