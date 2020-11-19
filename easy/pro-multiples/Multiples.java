78b8bfa01ab5a213c93deb9841a2cf2ce5e63d12
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-19 10:50:17 +0200

    Update Multiples.java

--- easy/pro-multiples/Multiples.java
+++ easy/pro-multiples/Multiples.java
@@ -3,6 +3,7 @@ public final class Multiples {
         System.out.print(java.util.stream.IntStream
             .range(3, new java.util.Scanner(System.in).nextInt())
             .filter(i -> i % 3 == 0 || i % 5 == 0)
-            .sum());
+            .sum()
+        );
     }
 }
