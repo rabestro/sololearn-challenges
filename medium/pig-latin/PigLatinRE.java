e94a141d6237de3b3b086ebebc9fbe2a617a0093
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-19 10:55:20 +0200

    Update PigLatinRE.java

--- medium/pig-latin/PigLatinRE.java
+++ medium/pig-latin/PigLatinRE.java
@@ -1,9 +1,9 @@
-public class PigLatinRE {
+public final class PigLatinRE {
     public static void main(String[] args) {
-
         System.out.println(
                 new java.util.Scanner(System.in)
                         .nextLine()
-                        .replaceAll("(\\S)(\\S*)", "$2$1ay"));
+                        .replaceAll("(\\S)(\\S*)", "$2$1ay")
+        );
     }
 }
