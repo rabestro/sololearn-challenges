1c56e242dcfdc8a2aaf77c89760bfac39302803c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-19 10:56:31 +0200

    Update PigLatinStream.java

--- medium/pig-latin/PigLatinStream.java
+++ medium/pig-latin/PigLatinStream.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
-public class PigLatinStream {
+public final class PigLatinStream {
     public static void main(String[] args) {
-
-        new Scanner(System.in)
+        new java.util.Scanner(System.in)
                 .tokens()
                 .map(word -> word.substring(1) + word.charAt(0) + "ay ")
                 .forEach(System.out::print);
