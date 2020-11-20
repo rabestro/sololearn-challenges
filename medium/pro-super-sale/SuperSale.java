898855cab6c6ba7d7a53d89273cab713a44ab59d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-21 00:30:08 +0200

    Update SuperSale.java

--- medium/pro-super-sale/SuperSale.java
+++ medium/pro-super-sale/SuperSale.java
@@ -1,17 +1,13 @@
-import java.util.Scanner;
-
 public class SuperSale {
-    public static void main(String[] args) {
-        final var in = new Scanner(System.in).useDelimiter(",");
-        float item, max = 0, sum = 0;
-        
-        while (in.hasNextFloat()) {
-        	item = in.nextFloat();
-        	if (item > max) max = item;
-        	sum += item;
-        }
-        final int totalSavings = (int) (0.3 * 1.07 * (sum - max));
-        
-        System.out.print(totalSavings);
-    }
+	public static void main(String[] args) {
+		System.out.print((int)(0.3 * 1.07 
+		   * -new java.util.Scanner(System.in)
+				.useDelimiter(",")
+				.tokens()
+				.mapToDouble(Double::parseDouble)
+				.map(x -> -x)
+				.sorted()
+				.skip(1)
+				.sum()));
+	}
 }
