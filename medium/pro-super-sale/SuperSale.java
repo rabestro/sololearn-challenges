fcbd49dbadc4d37f6f41e615bbf523da19bbd7c9
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-21 00:34:05 +0200

    Update SuperSale.java

--- medium/pro-super-sale/SuperSale.java
+++ medium/pro-super-sale/SuperSale.java
@@ -1,13 +1,14 @@
 public class SuperSale {
-	public static void main(String[] args) {
-		System.out.print((int)(0.3 * 1.07 
-		   * -new java.util.Scanner(System.in)
-				.useDelimiter(",")
-				.tokens()
-				.mapToDouble(Double::parseDouble)
-				.map(x -> -x)
-				.sorted()
-				.skip(1)
-				.sum()));
-	}
+    public static void main(String[] args) {
+        System.out.print((int)(0.3 * 1.07 
+                * -new java.util.Scanner(System.in)
+			.useDelimiter(",")
+			.tokens()
+			.mapToDouble(Double::parseDouble)
+			.map(x -> -x)
+			.sorted()
+			.skip(1)
+			.sum())
+	);
+    }
 }
