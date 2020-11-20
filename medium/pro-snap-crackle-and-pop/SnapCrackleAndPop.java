0103784889ac0249e8a90e4245030ec6ddc8866f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-20 21:12:25 +0200

    Update SnapCrackleAndPop.java

--- medium/pro-snap-crackle-and-pop/SnapCrackleAndPop.java
+++ medium/pro-snap-crackle-and-pop/SnapCrackleAndPop.java
@@ -1,12 +1,7 @@
-import java.util.Scanner;
-
 public class SnapCrackleAndPop {
     public static void main(String[] args) {
-        final var in = new java.util.Scanner(System.in);
-    
-        while(in.hasNextInt()) {
-        	final int i = in.nextInt();
-        	System.out.print(i % 3 > 0 ? i % 2 > 0 ? "Snap " : "Crackle " : "Pop ");
-        }
+        new java.util.Scanner(System.in).tokens().map(Integer::parseInt)
+                .map(i -> i % 3 > 0 ? i % 2 > 0 ? "Snap " : "Crackle " : "Pop ")
+                .forEach(System.out::print);
     }
 }
