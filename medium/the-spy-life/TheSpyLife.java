1e27f44957e868f335e7ba6f4dd4c6ec14d0a925
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-20 21:26:06 +0200

    Update TheSpyLife.java

--- medium/the-spy-life/TheSpyLife.java
+++ medium/the-spy-life/TheSpyLife.java
@@ -1,12 +1,7 @@
-import java.util.Scanner;
-
-public class TheSpyLife {
+public final class TheSpyLife {
     public static void main(String[] args) {
-        
-        final var input = new Scanner(System.in);
-        final var encoded = new StringBuilder(input.nextLine());
-        input.close();
-        
+        final var scanner = new java.util.Scanner(System.in);
+        final var encoded = new StringBuilder(scanner.nextLine());
         final var message = encoded.reverse().toString().replaceAll("[^a-zA-Z ]", "");
         
         System.out.print(message);
