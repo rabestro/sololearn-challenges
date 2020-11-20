b191bd61a725512072d5de503c9845fea9ad7004
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-20 21:19:04 +0200

    Update SnapCrackleAndPop.java

--- medium/pro-snap-crackle-and-pop/SnapCrackleAndPop.java
+++ medium/pro-snap-crackle-and-pop/SnapCrackleAndPop.java
@@ -1,6 +1,8 @@
 public class SnapCrackleAndPop {
     public static void main(String[] args) {
-        new java.util.Scanner(System.in).tokens().map(Integer::parseInt)
+        new java.util.Scanner(System.in)
+                .tokens()
+                .map(Integer::parseInt)
                 .map(i -> i % 3 > 0 ? i % 2 > 0 ? "Snap " : "Crackle " : "Pop ")
                 .forEach(System.out::print);
     }
