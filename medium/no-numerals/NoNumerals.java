0df436fc1dac4449a4b02c234371e249bf0fd55e
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-21 00:56:20 +0200

    Update NoNumerals.java

--- medium/no-numerals/NoNumerals.java
+++ medium/no-numerals/NoNumerals.java
@@ -1,10 +1,9 @@
 import java.util.Map;
 import java.util.Scanner;
 
-public class NoNumerals {
+public final class NoNumerals {
     public static void main(String[] args) {
-
-        final Map<String, String> numbers = Map.of(
+        final var numbers = Map.of(
                 "10", "ten", "1", "one", "2", "two", "3", "three", "4", "four",
                 "5", "five", "6", "six", "7", "seven", "8", "eight", "9", "nine");
 
