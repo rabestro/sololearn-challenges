ca96252baa71c42f8fb2c6aef624fdfcac24dcbf
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-21 08:44:16 +0200

    Update AverageWordLengthStream.java

--- medium/average-word-length/AverageWordLengthStream.java
+++ medium/average-word-length/AverageWordLengthStream.java
@@ -1,8 +1,7 @@
 import static java.util.stream.Collectors.averagingInt;
 
-public class AverageWordLengthStream {
+public final class AverageWordLengthStream {
     public static void main(String[] args) {
-
         System.out.print((int) Math.ceil(
                 new java.util.Scanner(System.in)
                         .useDelimiter("\\W+")
