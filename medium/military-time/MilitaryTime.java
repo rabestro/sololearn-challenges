e28a3ab6a634ce419a055c1904f0d3026e632459
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-21 08:38:47 +0200

    Update MilitaryTime.java

--- medium/military-time/MilitaryTime.java
+++ medium/military-time/MilitaryTime.java
@@ -4,18 +4,16 @@ import java.time.format.FormatStyle;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class MilitaryTime {
+public final class MilitaryTime {
     public static void main(String[] args) {
-        final var input = new Scanner(System.in);
-        final var usTime = input.nextLine();
-        input.close();
-
+        final var scanner = new Scanner(System.in);
+        final var usTime = scanner.nextLine();
+        
         final var formatter = DateTimeFormatter
                 .ofLocalizedTime(FormatStyle.SHORT)
                 .withLocale(Locale.US);
 
         final var euTime = LocalTime.parse(usTime, formatter);
-
         System.out.println(euTime);
     }
 }
