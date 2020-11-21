3bd245a2283ef7998177b94ac60ebd2be55390fe
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-21 08:32:57 +0200

    Update Divisible.java

--- medium/pro-divisible/Divisible.java
+++ medium/pro-divisible/Divisible.java
@@ -1,16 +1,9 @@
-public class Divisible {
+public final class Divisible {
     public static void main(String[] args) {
-        final var scan = new java.util.Scanner(System.in);
-        final var number = scan.nextInt();
-        scan.nextLine();
-
-        final var isDivisible = scan
-                .useDelimiter(" ")
-                .tokens()
-                .mapToInt(Integer::parseInt)
-                .allMatch(i -> number % i == 0);
-
-        System.out.print(isDivisible ? "divisible by all" : "not divisible by all");
-        scan.close();
+        final var scanner = new java.util.Scanner(System.in);
+        final var number = scanner.nextInt();
+        final var isDivisible = scanner.tokens().mapToInt(Integer::parseInt).allMatch(i -> number % i == 0); 
+           
+        System.out.println(isDivisible ? "divisible by all" : "not divisible by all");
     }
 }
