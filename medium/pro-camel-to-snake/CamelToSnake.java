5a7e5eb8116799d83e09ff5e55d4527243be4539
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-22 22:04:17 +0200

    Update CamelToSnake.java

--- medium/pro-camel-to-snake/CamelToSnake.java
+++ medium/pro-camel-to-snake/CamelToSnake.java
@@ -1,11 +1,7 @@
-public class CamelToSnake {
+public final class CamelToSnake {
     public static void main(String[] args) {
-        final var input = new java.util.Scanner(System.in);
-        final var codebase = input.nextLine();
-        input.close();
-
-        var output = codebase.replaceAll("(?<=[^//b^])([A-Z])", "_$1").toLowerCase();
-
+        final var codebase = new java.util.Scanner(System.in).nextLine();
+        final var output = codebase.replaceAll("(?<=[^//b^])([A-Z])", "_$1").toLowerCase();
         System.out.println(output);
     }
 }
