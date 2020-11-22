362aca1ea2a6e36bd62dabbcaf4ac09074133d49
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-22 22:22:08 +0200

    Update SplittingStrings.java

--- medium/pro-splitting-strings/SplittingStrings.java
+++ medium/pro-splitting-strings/SplittingStrings.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
-public final  class SplittingStrings {
+public final class SplittingStrings {
     public static void main(String[] args) {
         final var scanner = new Scanner(System.in);
         final var s = scanner.nextLine();
         final var n = scanner.nextInt();
-
+        
         final var separated = String.join("-", s.split(String.format("(?<=\\G.{%d})", n)));
-
         System.out.print(separated);
-    }
+   }
 }
