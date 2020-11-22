70e8753b0e8d14fa0e8f5a72ce3b8035e5d62f28
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-22 22:10:59 +0200

    Update SnowballingNumbers.java

--- medium/pro-snowballing-numbers/SnowballingNumbers.java
+++ medium/pro-snowballing-numbers/SnowballingNumbers.java
@@ -1,12 +1,14 @@
-public class SnowballingNumbers {
+public final class SnowballingNumbers {
     public static void main(String[] args) {
 
-        System.out.print(
+        System.out.println(
                 new java.util.Scanner(System.in)
                         .tokens()
                         .skip(1)
                         .mapToInt(Integer::parseInt)
                         .reduce((a, b) -> a = a > 0 && b > a ? a + b : 0)
-                        .orElse(0) > 0);
+                        .orElse(0)
+                        > 0
+        );
     }
 }
