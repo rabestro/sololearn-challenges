3c4b7eacde23d40173b4f60d02645e034f7f8672
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-22 22:08:30 +0200

    Update DaysBetweenDates.java

--- medium/pro-days-between-dates/DaysBetweenDates.java
+++ medium/pro-days-between-dates/DaysBetweenDates.java
@@ -9,13 +9,11 @@ import static java.time.temporal.ChronoUnit.DAYS;
 public class DaysBetweenDates {
     public static void main(final String[] args) {
         final var formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).withLocale(Locale.US);
-        final var input = new Scanner(System.in);
-        final var firstDate = LocalDate.parse(input.nextLine(), formatter);
-        final var secondDate = LocalDate.parse(input.nextLine(), formatter);
-        input.close();
-
+        final var scanner = new Scanner(System.in);
+        final var firstDate = LocalDate.parse(scanner.nextLine(), formatter);
+        final var secondDate = LocalDate.parse(scanner.nextLine(), formatter);
+  
         final long days = firstDate.until(secondDate, DAYS);
-
         System.out.println(days);
     }
 }
