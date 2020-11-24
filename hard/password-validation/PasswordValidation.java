3956de2c7dfe321aa41a9963c7d61f57770867c8
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-24 19:51:30 +0200

    Update PasswordValidation.java

--- hard/password-validation/PasswordValidation.java
+++ hard/password-validation/PasswordValidation.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
-public class PasswordValidation {
+public final class PasswordValidation {
     public static void main(String[] args) {
-        final var input = new Scanner(System.in);
-        final var password = input.nextLine();
-        input.close();
-        
+        final var scanner = new Scanner(System.in);
+        final var password = scanner.nextLine();
+	    
         final var isStrong = password.length() > 6
-           && Pattern.matches(".*\\d.*\\d.*", password)
-           && Pattern.matches("(.*[!@#$%&*].*){2,}", password);
+           && password.matches(".*\\d.*\\d.*")
+           && password.matches("(.*[!@#$%&*].*){2,}");
 	        
         System.out.println(isStrong ? "Strong" : "Weak");
     }
