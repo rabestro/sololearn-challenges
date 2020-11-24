411917317e677caf3f537c8df6b57f250571ca9e
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-24 17:54:05 +0200

    Update ItsSign.java

--- hard/pro-its-a-sign/ItsSign.java
+++ hard/pro-its-a-sign/ItsSign.java
@@ -1,11 +1,13 @@
 public class ItsSign {
     public static void main(String[] args) {
-	    
-        System.out.println(new java.util.Scanner(System.in)
-            .tokens()
-            .limit(4)
-            .map(StringBuilder::new)
-            .anyMatch(w -> w.toString().equals(w.reverse().toString())) 
-                ? "Open" : "Trash");
+        System.out.println(
+                new java.util.Scanner(System.in)
+                        .tokens()
+                        .limit(4)
+                        .map(StringBuilder::new)
+                        .anyMatch(w -> w.toString().equals(w.reverse().toString())) 
+                        ? "Open" 
+                        : "Trash"
+        );
     }
 }
