a2d94fa701e33da6a4afc9ebdedd84e979db9ede
Jegors Čemisovs <yegor.chemisov@gmail.com>
2022-05-26 22:28:49 +0300

    Update Map2D.java

--- hard/pro-2d-map/Map2D.java
+++ hard/pro-2d-map/Map2D.java
@@ -4,14 +4,12 @@ public class Map2D {
     public final static int SIZE = 5 + 1; // Size of square + comma
     
     public static void main(String[] args) {
-        final var input = new Scanner(System.in);
-        final var map = input.nextLine();
-        input.close();
+        var map = new Scanner(System.in).nextLine();
         
-        final int indexA = map.indexOf('P');
-        final int indexB = map.indexOf('P', indexA + 1);
+        int indexA = map.indexOf('P');
+        int indexB = map.indexOf('P', indexA + 1);
         
-        final int path = indexB / SIZE - indexA / SIZE + Math.abs(indexA % SIZE - indexB % SIZE);
+        int path = indexB / SIZE - indexA / SIZE + Math.abs(indexA % SIZE - indexB % SIZE);
         
         System.out.print(path);
     }
