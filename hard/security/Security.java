df3006afc73818f4a419b256c0b45dfd2d6bb71f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2022-05-26 22:29:48 +0300

    Update Security.java

--- hard/security/Security.java
+++ hard/security/Security.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
-public final class Security {
+public class Security {
     public static void main(final String[] args) {
-        final var floor = new Scanner(System.in).nextLine();
-        
-        final var isSafe = floor.matches(".*[$T].*G.*[$T].*");
+        var floor = new Scanner(System.in).nextLine();
+        var isSafe = floor.matches(".*[$T].*G.*[$T].*");
 
         System.out.println(isSafe ? "quiet" : "ALARM");
     }
