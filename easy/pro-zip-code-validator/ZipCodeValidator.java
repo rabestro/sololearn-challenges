b3a3965146b38b05dc962cff809149b577f6e7bc
Jegors Čemisovs <yegor.chemisov@gmail.com>
2022-05-27 17:12:46 +0300

    Update ZipCodeValidator.java

--- easy/pro-zip-code-validator/ZipCodeValidator.java
+++ easy/pro-zip-code-validator/ZipCodeValidator.java
@@ -1,8 +1,8 @@
-public final class ZipCodeValidator {
+public class ZipCodeValidator {
     public static void main(String[] args) {
-        final var scanner = new java.util.Scanner(System.in);
-        final var zipCode = scanner.nextLine();
-        final var isValid = zipCode.matches("\\d{5}");
+        var scanner = new java.util.Scanner(System.in);
+        var zipCode = scanner.nextLine();
+        var isValid = zipCode.matches("\\d{5}");
         System.out.println(isValid);
     }
 }
